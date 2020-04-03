95bc266256162ad5364686f1efae0b80ea898b87
aeongrp <aeongrp@outlook.com>
2020-04-03 06:48:42 +0000

    Added driver menu name in config file

---
+++ models/files/scmanagecomm.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+contract ManageCommittee{
+    // nodeIds: bytes of concatenated NodeIDs
+    // pubs: bytes of concatenated public keys
+    // sigs: bytes of concatenated signatures of hash(sender, nonce, nodeIds), one-to-one correspondence with pubs
+    // returns the number of nodes added
+    function addNodes(address sender, uint64 nonce, bytes memory nodeIds, bytes memory pubs, bytes memory sigs) public returns(bool status, uint8 delta){}
+
+    // nodeIds: bytes of concatenated NodeIDs
+    // pubs: bytes of concatenated public keys
+    // sigs: bytes of concatenated signatures of hash(sender, nonce, nodeIds), one-to-one correspondence with pubs
+    // returns the number of nodes deleted
+    function delNodes(address sender, uint64 nonce, bytes memory nodeIds, bytes memory pubs, bytes memory sigs) public returns(bool status, uint8 delta){}
+
+    // returns bytes of concatenated all NodeIDs
+    function listNodes() public returns(bytes memory nodeIds){}
+}
