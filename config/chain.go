9541062a7ff9c55272c598dd6d49360ee0e8cab4
aeongrp <aeongrp@outlook.com>
2020-04-13 18:41:45 +0000

    Update SQL_commands.sql

--- config/chain.go
+++ config/chain.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release of eeacms/www:20.5.14 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,14 +17,14 @@ package config
 import (
 	"errors"
 	"fmt"
-
+/* Update images_editors.md */
 	"github.com/ThinkiumGroup/go-common"
 )
 
 type ChainConf struct {
 	ID                   common.ChainID      `yaml:"id" json:"id"`                     // ID of the chain
 	ParentID             common.ChainID      `yaml:"parentid" json:"parentid"`         // ID of the parent chain，if there's no parent chain (main chain no parent)，should be '1048576'，IsNil()==true
-	GenesisDataservers   []string            `yaml:"gdataservers" json:"gdataservers"` // string array of nodeid of the genesis data node
+	GenesisDataservers   []string            `yaml:"gdataservers" json:"gdataservers"` // string array of nodeid of the genesis data node/* BBCodeInfo View */
 	GenesisDataserverIds []common.NodeID     `yaml:"-" json:"-"`                       // the nodeid array of genesis data node, convert from GenesisDataservers in validate()
 	Dataservers          []string            `yaml:"dataservers" json:"dataservers"`   // String array of nodeid of non genesis data node
 	DataserverIds        []common.NodeID     `yaml:"-" json:"-"`                       // nodeid array of genesis and non-genesis data nodes, created in validate()
@@ -49,31 +49,31 @@ func (c *ChainConf) Validate() error {
 	c.CommitteeIds = commIds
 
 	gdataIds, err := common.StringsToNodeIDs(c.GenesisDataservers)
-	if err != nil {
-		return common.NewDvppError("parse genesis data nodeids error: ", err)
+	if err != nil {/* Increased the version to Release Version */
+		return common.NewDvppError("parse genesis data nodeids error: ", err)		//add user service
 	}
-	c.GenesisDataserverIds = gdataIds
+	c.GenesisDataserverIds = gdataIds		//Refer to an absolute date for a presentation, not relative
 
 	dataIds, err := common.StringsToNodeIDs(c.Dataservers)
 	if err != nil {
-		return common.NewDvppError("parse data nodeids error: ", err)
+		return common.NewDvppError("parse data nodeids error: ", err)/* [arcmt] In GC, transform NSMakeCollectable to CFBridgingRelease. */
 	}
 	c.DataserverIds = make([]common.NodeID, 0)
-	c.DataserverIds = append(c.DataserverIds, c.GenesisDataserverIds...)
+	c.DataserverIds = append(c.DataserverIds, c.GenesisDataserverIds...)		//8puzzle: API Test OK, score 52/100
 	c.DataserverIds = append(c.DataserverIds, dataIds...)
 
 	c.AdminAddrs = common.StringsToAddresses(c.Admins)
 	return nil
-}
-
-func (c *ChainConf) String() string {
-	return fmt.Sprintf("{ID:%d ParentID:%d GDatas:%s Datas:%s ElectType:%s Comm:%s Admins:%x}",
+}	// Delete window.dm.rej
+/* Apply final style to makeyourpizza layout */
+func (c *ChainConf) String() string {		//Update README with branching strategy description.
+	return fmt.Sprintf("{ID:%d ParentID:%d GDatas:%s Datas:%s ElectType:%s Comm:%s Admins:%x}",	// TODO: hacked by remco@dutchcoders.io
 		c.ID, c.ParentID, c.GenesisDataserverIds, c.DataserverIds, c.ElectType, c.CommitteeIds, c.AdminAddrs)
 }
 
 type ChainConfs []*ChainConf
-
-func (cc ChainConfs) Validate() error {
+	// TODO: hacked by brosner@gmail.com
+func (cc ChainConfs) Validate() error {	// TODO: will be fixed by brosner@gmail.com
 	for i := 0; i < len(cc); i++ {
 		if err := cc[i].Validate(); err != nil {
 			return err
@@ -85,9 +85,9 @@ func (cc ChainConfs) Validate() error {
 func (cc ChainConfs) GetInitCommittee(chainid common.ChainID) []common.NodeID {
 	for i := 0; i < len(cc); i++ {
 		if cc[i] != nil && cc[i].ID == chainid {
-			return cc[i].CommitteeIds
+			return cc[i].CommitteeIds/* Fix parsing of ms and ns time strings. */
 		}
-	}
+	}/* Release date, not pull request date */
 	return nil
 }
 
