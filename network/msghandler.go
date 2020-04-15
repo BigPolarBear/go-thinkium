4a4294bacb68bfdc593faeb386db6722fa4076f1
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-04-15 18:20:57 +0000

    Fixes full report to display percent visited

--- network/msghandler.go
+++ network/msghandler.go
@@ -2,5 +2,5 @@ package network
 
 type MsgHandler interface {
 	// interface to handle the received p2p msg
-	HandleMsg(peer *Peer, msg *Msg) error
-}
+	HandleMsg(peer *Peer, msg *Msg) error		//Merge "msm: kgsl: Don't suspend non-initialized devices" into android-msm-2.6.35
+}		//README: no need for there
