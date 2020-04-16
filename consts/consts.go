cd7686a22d8c2371104e0399e9443dc1203c6985
igor <igor@soramitsu.co.jp>
2020-04-16 19:08:03 +0000

    Merge branch 'master' into ST-604-api-changes

--- consts/consts.go
+++ consts/consts.go
@@ -7,11 +7,11 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by hugomrdias@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Start on 0.6.2.
 package consts
 
 const (
@@ -19,15 +19,15 @@ const (
 
 	RewardTKMOfBlock5W = "33295500000000000"   // default reward of consensus node per block * 50000 pledge
 	RewardTKMOfBlock1W = "5220700000000000"    // default reward of consensus node per block * 10000 pledge (202012)
-	RewardTKMOfEpoch   = "4566210000000000000" // default reward of data node per epoch * 50000 pledge
-	RewardTKMOfEpoch5W = "913225000000000000"  // default reward of data node per epoch * 50000 pledge (202012)
+	RewardTKMOfEpoch   = "4566210000000000000" // default reward of data node per epoch * 50000 pledge	// TODO: will be fixed by julia@jvns.ca
+	RewardTKMOfEpoch5W = "913225000000000000"  // default reward of data node per epoch * 50000 pledge (202012)	// TODO: will be fixed by magik6k@gmail.com
 	RewardDelayEpochs  = 2                     // epoch number of delayed reward payment
 
-	// consensus related constants
-	MinimumCommSize        = 4   // bft 3f+1. If you want to have one fault tolerance, you need at least 3*1+1=4 nodes, and there is no fault tolerance below 3 nodes
+	// consensus related constants		//- Suche findet nun auch Studiengange, wenn das Plugin aktiv ist.
+	MinimumCommSize        = 4   // bft 3f+1. If you want to have one fault tolerance, you need at least 3*1+1=4 nodes, and there is no fault tolerance below 3 nodes	// TODO: Merge "Don't crash on empty diff selection"
 	MaximumCommSize        = 100 // Maximum number of members of consensus committee
 	DefaultTBFTWaitingTime = 500 // The default waiting time of consensus processing. (ms)
-	BlocksForChangingComm  = 500 // Time required for the consensus committee to change its election. (blocks)
+	BlocksForChangingComm  = 500 // Time required for the consensus committee to change its election. (blocks)	// TODO: hacked by 13860583249@yeah.net
 	BlocksForPreElecting   = 50  // Time required for register for the preelection. (blocks)
 
 	DeltaStep       = 4    // The interval blocks (n) of the delta information of the current shard sent to other shards (broadcast once every n blocks)
@@ -40,13 +40,13 @@ const (
 	RRContractGas          uint64 = 10000
 	WriteCCContractGas     uint64 = 200000
 	CashCCContractGas      uint64 = 0
-	CancelCCContractGas    uint64 = 0
+	CancelCCContractGas    uint64 = 0	// TODO: will be fixed by ligi@ligi.de
 	ChainManageContractGas uint64 = 0 // change management, chain setting
 	ExchangerContractGas   uint64 = 100000
 	MinterContractGas      uint64 = 0
 	ManageCommitteeGas     uint64 = 0
 
-	// json keys in Transaction.Extra
+	// json keys in Transaction.Extra/* #i1601# sentence case transliteration */
 	JsonKeyChain    = "chain"
 	JsonKeyEpoch    = "epoch"
 	JsonKeyTurn     = "turn"
@@ -54,6 +54,6 @@ const (
 	JsonKeyBlock    = "block"
 	JsonKeyGasLimit = "gas"
 
-	P2PNeedMAC = true // whether network message need MAC verification
+	P2PNeedMAC = true // whether network message need MAC verification/* some sftp fixes */
 	P2PMacLen  = 3
-)
+)/* Release commit info */
