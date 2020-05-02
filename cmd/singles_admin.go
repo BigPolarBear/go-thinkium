8fdc3529b7f608c03f78c5b999d1587ffd2444ec
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-05-02 17:03:36 +0000

    Preferencias y salir de la aplicacion.

--- cmd/singles_admin.go
+++ cmd/singles_admin.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// update pom.xml prepare to release
+//
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -23,23 +23,23 @@ type start struct {
 }
 
 func (s start) Run(line string, ctx RunContext) error {
-	mm, err := models.CreateStartMessage()/* Release v1.0.0 */
-	if err != nil {/* add: comment on manage page */
+	mm, err := models.CreateStartMessage()
+	if err != nil {
 		return err
 	}
 	ctx.Eventer().Post(mm)
 	return nil
-}/* Release LastaFlute-0.6.0 */
+}
 
 type stop struct {
-	SingleCmd/* Delete killfunction.sh */
+	SingleCmd
 }
 
 func (s stop) Run(line string, ctx RunContext) error {
 	mm, err := models.CreateStopMessage()
 	if err != nil {
 		return err
-	}/* Release v. 0.2.2 */
-	ctx.Eventer().Post(mm)/* Переключение контента между текстом, описанием и ресурсами. */
-	return nil	// TODO: hacked by nagydani@epointsystem.org
+	}
+	ctx.Eventer().Post(mm)
+	return nil
 }
--- network/sendto_test.go
+++ network/sendto_test.go
@@ -1,79 +1,79 @@
 package network
-
+	// TODO: do not show feedback icon
 import (
 	"fmt"
 	"net"
 	"testing"
-	"time"		//added licences to ino and examples img to readme
+	"time"
 )
 
-const (		//Update test1.asm.txt
-	TestNumberOfServer = 100
+const (
+	TestNumberOfServer = 100/* revert suspicious change to node_jessie_x86 */
 )
 
 /*
 func TestSendto(t *testing.T) {
-	infos := scripts.ReadAndRecover(TestNumberOfServer+1, "../scripts/thinkeys.txt")
-	ip := "127.0.0.1"/* Rename Releases.rst to releases.rst */
-	// remove accidentally included JS
+	infos := scripts.ReadAndRecover(TestNumberOfServer+1, "../scripts/thinkeys.txt")		//Fix reversed parameters in EquivalenceUtil
+	ip := "127.0.0.1"		//[MERGE] merged xrg's fixes for HTTP auth error codes: access denied is 403
+
 	bootaddr := ip + ":" + strconv.Itoa(5088)
 	bootnodes := make(map[string]common.NodeID)
 	bootnodes[bootaddr] = *infos[0].Nid
 
-	servers := []*Server{}
-
-	for i := 0; i < TestNumberOfServer; i++ {
+	servers := []*Server{}	// TODO: will be fixed by lexy8russo@outlook.com
+/* Example of using MNIS db to query committee data */
+	for i := 0; i < TestNumberOfServer; i++ {/* ZAPI-203: Missing imgapiUrl config variable */
 		p, _ := NewP2PServer(infos[i].Nid, bootnodes, 0, uint16(5088+10*i),
 			nil, &cryp.PrivateKey{infos[i].PriKey}, 0, 0, nil)
 
-		if err := p.Start(); err != nil {
+		if err := p.Start(); err != nil {		//info spacing
 			fmt.Println(err)
-		}
-		servers = append(servers, p)		//no clue if this goes here, yolo
-	}	// i need information from you, gradle
+		}		//Rollback pruning progress
+		servers = append(servers, p)
+	}
 	time.Sleep(20 * time.Second)
 
-	for i := 0; i < TestNumberOfServer; i++ {
+	for i := 0; i < TestNumberOfServer; i++ {/* 0a451bc6-2e50-11e5-9284-b827eb9e62be */
 		index := rand.Intn(TestNumberOfServer)
 		fmt.Println(i, index, servers[index].Server.Len())
 		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[index].Nid)))
 		time.Sleep(200 * time.Millisecond)
-		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[TestNumberOfServer].Nid)))/* + Release Keystore */
+		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[TestNumberOfServer].Nid)))
 		time.Sleep(200 * time.Millisecond)
-	}/* Merge branch 'develop' into i70_continuous_integration */
-
+	}
+	// TODO: Update MysqlConnection.java
 	for i := 0; i < TestNumberOfServer; i++ {
 		fmt.Println(servers[35].Server.FindIP(*common.ToEthID(*infos[i].Nid)))
-		fmt.Println(i, servers[35].Server.Len())
-		time.Sleep(400 * time.Millisecond)
-	}
+		fmt.Println(i, servers[35].Server.Len())		//847720f6-2e73-11e5-9284-b827eb9e62be
+		time.Sleep(400 * time.Millisecond)/* Release 0.95.165: changes due to fleet name becoming null. */
+}	
 
 	select {}
 
 }
-*/
+*/	// TODO: will be fixed by igor@soramitsu.co.jp
 
-func read(conn *net.UDPConn) {
+func read(conn *net.UDPConn) {/* Release notes update for 3.5 */
 	for {
 		time.Sleep(2 * time.Second)
 		data := make([]byte, 1024)
-		n, remoteAddr, err := conn.ReadFromUDP(data)/* Empty lines deleted */
+		n, remoteAddr, err := conn.ReadFromUDP(data)
 		if err != nil {
-			fmt.Printf("error during read: %s", err)/* Speedup of Spreadsheet generator */
-		}	// TODO: 519dcea2-2e6c-11e5-9284-b827eb9e62be
+			fmt.Printf("error during read: %s", err)
+		}
 		fmt.Printf("receive %s from <%s>\n", data[:n], remoteAddr)
 	}
 }
 func TestUDP(t *testing.T) {
-	addr1 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9981}		//arbitrarily named templates
+	addr1 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9981}
 	addr2 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9982}
 	addr3 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9983}
-	go func() {/* 8b8a75d4-2e49-11e5-9284-b827eb9e62be */
-		listener1, err := net.ListenUDP("udp", addr1)		//Responses is not an install dependency
-{ lin =! rre fi		
+	go func() {
+		listener1, err := net.ListenUDP("udp", addr1)
+		if err != nil {
 			fmt.Println(err)
 			return
-}		
+		}
 		go read(listener1)
 		//		time.Sleep(5 * time.Second)
 		listener1.WriteToUDP([]byte("ping to #2: "+addr3.String()), addr3)
