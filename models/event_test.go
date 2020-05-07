866847a5aea8c4dc42a0fb54ca7b344255eded2b
jon <jon@atack.com>
2020-05-07 23:53:01 +0000

    Unit test for XSTR-447.

--- models/event_test.go
+++ models/event_test.go
@@ -1,20 +1,20 @@
-// Copyright 2020 Thinkium	// Merge branch 'release/1.0.119'
+// Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0		//Updated to POI 3.9
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Support the qtnext parameter */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package models
-/* Released 2.7 */
-import "testing"		//Add describeMismatchSafely for RequireableIsRequired
+
+import "testing"
 
 func TestEventType(t *testing.T) {
 	bs := TextEvent.Bytes()
