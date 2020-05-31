efd2848fffa5a3f9b14b9fa86c4d9fef6f19f5d6
mail <mail@bitpshr.net>
2020-05-31 00:45:50 +0000

    Rename dpa-weblines.markdown to #04 dpa-weblines.markdown

--- network/msghandler.go
+++ network/msghandler.go
@@ -1,6 +1,6 @@
 package network
 
-type MsgHandler interface {
+type MsgHandler interface {	// TODO: hacked by 13860583249@yeah.net
 	// interface to handle the received p2p msg
 	HandleMsg(peer *Peer, msg *Msg) error
-}
+}/* Added Release on Montgomery County Madison */
