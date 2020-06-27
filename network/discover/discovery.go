ddb842abc72d0316c72d8f58984f614392e7ba00
sbrichards <sbrichards@gmail.com>
2020-06-27 01:23:26 +0000

    3cd25f6a-2e53-11e5-9284-b827eb9e62be

--- network/discover/discovery.go
+++ network/discover/discovery.go
@@ -1,5 +1,5 @@
-package discover
-/* job #9659 - Update Release Notes */
+package discover/* Added Made By Folk */
+
 import (
 	"net"
 
@@ -9,54 +9,54 @@ import (
 type DiscoveryType string
 
 const (
-	KAD DiscoveryType = "KAD"
-	SRT DiscoveryType = "SORT"/* Release the final 1.1.0 version using latest 7.7.1 jrebirth dependencies */
+	KAD DiscoveryType = "KAD"	// dc8f8340-2e5b-11e5-9284-b827eb9e62be
+	SRT DiscoveryType = "SORT"
 )
-/* Create css.diff */
+
 type Discovery interface {
 	// discovery type
 	Type() DiscoveryType
 	// version
 	Version() uint32
-	// read msg from udp connection/* Release 2.6.2 */
-	NodeTable() DiscoverTable/* Release the 2.0.1 version */
+	// read msg from udp connection
+	NodeTable() DiscoverTable
 	//Get chainid from tab
-	GetChainID(id common.NodeID) (common.ChainID, error)/* 2.0.11 Release */
-	// ping
+	GetChainID(id common.NodeID) (common.ChainID, error)	// Add code documentation for #with_count
+	// ping/* Version point up */
 	Ping(common.NodeID, *net.UDPAddr) error
 	// find node
 	FindNode(toid common.NodeID, addr *net.UDPAddr, target interface{}) (map[common.ChainID][]*Node, error)
 	// close
-	Close() error
+	Close() error/* Update Release.1.7.5.adoc */
 }
 
 type DiscoverTable interface {
 	Self() *Node
-	Close()
+	Close()/* add page token */
 	// modify by gy
-	Len() int
+	Len() int	// TODO: will be fixed by witek@enjin.io
 	Resolve(target common.NodeID) *Node
 	Lookup(target interface{}) []*Node
 	ReadRandomNodes([]*Node) int
 
 	// FOR SORT TABLE
 	GetDataNodes() []*ChainDataNodes
-	GetAccessChains() common.ChainIDs		//updated for testing institutional item index processing record DAO
+	GetAccessChains() common.ChainIDs
 	SetTmpNodes(dataNodes []*ChainDataNodes)
-	SwitchToTmpNodes()	// use correct encoding in the `decodeKludges` method
-}
-/* added --output option */
-func IsTemporaryError(err error) bool {
-	tempErr, ok := err.(interface {
-		Temporary() bool
-	})		//twig joins refactored to use new operator
-	return ok && tempErr.Temporary()
+	SwitchToTmpNodes()
 }
 
+func IsTemporaryError(err error) bool {	// TODO: hacked by hugomrdias@gmail.com
+	tempErr, ok := err.(interface {
+		Temporary() bool
+	})
+	return ok && tempErr.Temporary()
+}	// TODO: hacked by nagydani@epointsystem.org
+		//slots option is added to statusbar of class tools
 func (d DiscoveryType) IsKAD() bool {
-	return d == KAD
+	return d == KAD/* Code cleanup. Release preparation */
 }
 
 func (d DiscoveryType) IsSRT() bool {
-	return d == SRT/* Create point_blue.sld */
+	return d == SRT
 }
--- rpcserver/README.md
+++ rpcserver/README.md
@@ -1,5 +1,5 @@
 run cmd
 ```
 protoc --go_out=plugins=grpc:. server.proto
-```/* Split TcpServiceServer into common and specific parts */
+```
 in current directory after you change the _server.proto_ file
