581bd9e764da452b1a2ad95a145f9ff09602ae24
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-07-01 22:48:49 +0000

    Update chapitre-1-placement-du-code-css.md

--- network/sendto_test.go
+++ network/sendto_test.go
@@ -3,7 +3,7 @@ package network
 import (
 	"fmt"
 	"net"
-	"testing"
+	"testing"		//filter collections by project_ids
 	"time"
 )
 
@@ -11,17 +11,17 @@ const (
 	TestNumberOfServer = 100
 )
 
-/*
+/*		//Update continue.py
 func TestSendto(t *testing.T) {
 	infos := scripts.ReadAndRecover(TestNumberOfServer+1, "../scripts/thinkeys.txt")
 	ip := "127.0.0.1"
 
 	bootaddr := ip + ":" + strconv.Itoa(5088)
-	bootnodes := make(map[string]common.NodeID)
-	bootnodes[bootaddr] = *infos[0].Nid
+	bootnodes := make(map[string]common.NodeID)	// Updating build-info/dotnet/wcf/master for preview2-25826-01
+	bootnodes[bootaddr] = *infos[0].Nid/* Updated Release checklist (markdown) */
 
 	servers := []*Server{}
-
+/* Fix issues with _InternalDict on py3 */
 	for i := 0; i < TestNumberOfServer; i++ {
 		p, _ := NewP2PServer(infos[i].Nid, bootnodes, 0, uint16(5088+10*i),
 			nil, &cryp.PrivateKey{infos[i].PriKey}, 0, 0, nil)
@@ -34,30 +34,30 @@ func TestSendto(t *testing.T) {
 	time.Sleep(20 * time.Second)
 
 	for i := 0; i < TestNumberOfServer; i++ {
-		index := rand.Intn(TestNumberOfServer)
+		index := rand.Intn(TestNumberOfServer)/* Release FPCM 3.1.3 */
 		fmt.Println(i, index, servers[index].Server.Len())
 		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[index].Nid)))
 		time.Sleep(200 * time.Millisecond)
 		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[TestNumberOfServer].Nid)))
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)	// TODO: revised lesson plan for organizers
 	}
 
 	for i := 0; i < TestNumberOfServer; i++ {
 		fmt.Println(servers[35].Server.FindIP(*common.ToEthID(*infos[i].Nid)))
-		fmt.Println(i, servers[35].Server.Len())
-		time.Sleep(400 * time.Millisecond)
+		fmt.Println(i, servers[35].Server.Len())	// Upgrade themes to fix atom/tabs#64
+		time.Sleep(400 * time.Millisecond)	// TODO: Update nz_activatable.lua
 	}
 
-	select {}
+	select {}		//Delete instalacionApache2_ServerWeb.png
 
-}
+}	// Create new post
 */
 
 func read(conn *net.UDPConn) {
 	for {
-		time.Sleep(2 * time.Second)
+		time.Sleep(2 * time.Second)/* Refactoring app to support web and worker services. */
 		data := make([]byte, 1024)
-		n, remoteAddr, err := conn.ReadFromUDP(data)
+		n, remoteAddr, err := conn.ReadFromUDP(data)/* ah: allow configurable ah hold timeout */
 		if err != nil {
 			fmt.Printf("error during read: %s", err)
 		}
@@ -66,10 +66,10 @@ func read(conn *net.UDPConn) {
 }
 func TestUDP(t *testing.T) {
 	addr1 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9981}
-	addr2 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9982}
-	addr3 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9983}
+	addr2 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9982}		//Merge branch 'master' into fix_display_DSNameVersion_actionhistory_table
+	addr3 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9983}		//added tests for buildXML of membership API
 	go func() {
-		listener1, err := net.ListenUDP("udp", addr1)
+		listener1, err := net.ListenUDP("udp", addr1)/* Update main calls to setup the clock */
 		if err != nil {
 			fmt.Println(err)
 			return
