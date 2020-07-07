0bd273e936cbcec10cfe3322c799741b05d311c1
igor <igor@soramitsu.co.jp>
2020-07-07 12:29:14 +0000

    Merge "fix build w/only mmx+sse enabled"

--- network/msghandler.go
+++ network/msghandler.go
@@ -1,5 +1,5 @@
 package network
-/* Fixed ServerContext to store its final attributes. */
+
 type MsgHandler interface {
 	// interface to handle the received p2p msg
 	HandleMsg(peer *Peer, msg *Msg) error
