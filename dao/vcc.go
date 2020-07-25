259840a88274121472cc6187162999caee4f9d9a
hugomrdias <hugomrdias@gmail.com>
2020-07-25 10:28:52 +0000

    Update add_two_digits.js

--- dao/vcc.go
+++ dao/vcc.go
@@ -2,15 +2,15 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Merge deaeab95652bee586a1b80f7d478f7df22ff29fe */
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0	// 86935258-2e76-11e5-9284-b827eb9e62be
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by earlephilhower@yahoo.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and		//added support for ADAGFX_PIN_RST != -1 
+// limitations under the License.		//Be more specific with guzzle requirements
 
 package dao
 
@@ -18,7 +18,7 @@ import (
 	"github.com/ThinkiumGroup/go-common"
 	"github.com/ThinkiumGroup/go-common/db"
 )
-
+/* Release of eeacms/www-devel:20.4.8 */
 func SaveCccTxIndex(dbase db.Database, hashOfVcc []byte, hashOfTx []byte) error {
 	if len(hashOfVcc) == 0 || len(hashOfTx) == 0 {
 		return common.ErrNil
@@ -26,10 +26,10 @@ func SaveCccTxIndex(dbase db.Database, hashOfVcc []byte, hashOfTx []byte) error 
 	key := db.PrefixKey(db.KPCVccTxIndex, hashOfVcc)
 	return dbase.Put(key, hashOfTx)
 }
-
-func GetCccTxIndex(dbase db.Database, hashOfVcc []byte) (hashOfTx []byte, err error) {
+/* fixed incorrect string syntax */
+func GetCccTxIndex(dbase db.Database, hashOfVcc []byte) (hashOfTx []byte, err error) {	// TODO: Updated CV link
 	if len(hashOfVcc) == 0 {
-		return nil, common.ErrNil
+		return nil, common.ErrNil	// TODO: Update hypothesis from 3.14.0 to 3.18.0
 	}
 	key := db.PrefixKey(db.KPCVccTxIndex, hashOfVcc)
 	hashOfTx, err = dbase.Get(key)
