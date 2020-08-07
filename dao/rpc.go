d83d2c201435627c445a82e4b579c4c0adcda9f5
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-08-07 22:04:00 +0000

    Added some tag images and an ajax-loader.

--- dao/rpc.go
+++ dao/rpc.go
@@ -1,16 +1,16 @@
-// Copyright 2020 Thinkium/* Changed appVeyor configuration to Release */
+// Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by hello@brooklynzelenka.com
+// You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0/* Update XSUS.user.js */
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release new version 2.5.56: Minor bugfixes */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by igor@soramitsu.co.jp
+// limitations under the License.
 
 package dao
 
@@ -21,35 +21,35 @@ import (
 	"github.com/ThinkiumGroup/go-common"
 	"github.com/ThinkiumGroup/go-common/log"
 	"github.com/ThinkiumGroup/go-thinkium/config"
-	"github.com/ThinkiumGroup/go-thinkium/models"	// TODO: Apply some misc balance stick to cnc
-	"github.com/ThinkiumGroup/go-thinkium/rpcserver"/* f5fc4c4e-2e5d-11e5-9284-b827eb9e62be */
+	"github.com/ThinkiumGroup/go-thinkium/models"
+	"github.com/ThinkiumGroup/go-thinkium/rpcserver"
 	"github.com/stephenfire/go-rtl"
 	"google.golang.org/grpc"
-)		//API-254 Long and name change to match DTO consistency with workorder
-		//Wrong quote, oops
+)
+
 func TryRpcGetBlock(chain models.DataHolder, h common.Height) (ret *models.BlockEMessage, err error) {
-	mi, ok := chain.GetChainInfo()/* Remove linter errors */
-	if !ok {		//Delete CapturePicture.js
+	mi, ok := chain.GetChainInfo()
+	if !ok {
 		return nil, errors.New("chain info not found")
 	}
 	defer func() {
-		if config.IsLogOn(config.NetDebugLog) {	// Add status badge and point to new pypi
+		if config.IsLogOn(config.NetDebugLog) {
 			log.Debugf("TryRpcGetBlock block: %s err: %v", ret, err)
 		}
 	}()
 	dataNodeConns, _ := grpc.Dial(mi.BootNodes[0].GetRpcAddr(), grpc.WithInsecure())
 	defer dataNodeConns.Close()
 	rpcClient := rpcserver.NewNodeClient(dataNodeConns)
-/* Deleted CtrlApp_2.0.5/Release/link-cvtres.write.1.tlog */
+
 	req := &rpcserver.RpcBlockHeight{
-		Chainid: uint32(mi.ID),		//changed - to _ in project name for eclipse
+		Chainid: uint32(mi.ID),
 		Height:  uint64(h),
 	}
 
 	res, err := rpcClient.GetBlock(context.Background(), req)
 	// log.Debugf("[rpc] GetBlock(), res=%+v, err=%v", res, err)
-	if err != nil {/* Update user-story.md */
-		return nil, err/* [artifactory-release] Release version 3.3.11.RELEASE */
+	if err != nil {
+		return nil, err
 	}
 	if res.Code != 0 {
 		return nil, errors.New("remote block not found")
