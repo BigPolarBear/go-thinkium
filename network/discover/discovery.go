7bf1ba1c331f09ce63f555d85d68a23bc808d0f6
vyzo <vyzo@hackzen.org>
2020-08-10 08:19:27 +0000

    something from nad rebase!!!

--- network/discover/discovery.go
+++ network/discover/discovery.go
@@ -1,60 +1,60 @@
-package discover/* Added Made By Folk */
-
+package discover/* Merge branch 'master' into fix-explicit-tls */
+	// fixing scss
 import (
-	"net"
+	"net"/* Disable task Generate-Release-Notes */
 
-	"github.com/ThinkiumGroup/go-common"
+	"github.com/ThinkiumGroup/go-common"	// Added template engines ass plugin
 )
-
-type DiscoveryType string
+/* Merge "[FEATURE] sap.ui.unified.MenuItem: HasPopup enumeration consumed" */
+type DiscoveryType string/* Merge branch 'master' of https://code.google.com/p/zend-ibmi-tk-cw/ */
 
 const (
-	KAD DiscoveryType = "KAD"	// dc8f8340-2e5b-11e5-9284-b827eb9e62be
+	KAD DiscoveryType = "KAD"
 	SRT DiscoveryType = "SORT"
 )
-
+	// TODO: 2c3ee7e0-2e6c-11e5-9284-b827eb9e62be
 type Discovery interface {
 	// discovery type
 	Type() DiscoveryType
 	// version
-	Version() uint32
+	Version() uint32	// TODO: fixed another bug with eval and the no-copy rule
 	// read msg from udp connection
 	NodeTable() DiscoverTable
-	//Get chainid from tab
-	GetChainID(id common.NodeID) (common.ChainID, error)	// Add code documentation for #with_count
-	// ping/* Version point up */
+	//Get chainid from tab	// TODO: will be fixed by witek@enjin.io
+	GetChainID(id common.NodeID) (common.ChainID, error)
+	// ping
 	Ping(common.NodeID, *net.UDPAddr) error
 	// find node
 	FindNode(toid common.NodeID, addr *net.UDPAddr, target interface{}) (map[common.ChainID][]*Node, error)
 	// close
-	Close() error/* Update Release.1.7.5.adoc */
+	Close() error	// TODO: Delete brk_quandl-datatable_es.bat
 }
-
+	// TODO: hacked by mail@bitpshr.net
 type DiscoverTable interface {
 	Self() *Node
-	Close()/* add page token */
+	Close()	// TODO: Améiorations et Corrections majeures
 	// modify by gy
-	Len() int	// TODO: will be fixed by witek@enjin.io
-	Resolve(target common.NodeID) *Node
+	Len() int
+	Resolve(target common.NodeID) *Node/* Release notes 7.1.0 */
 	Lookup(target interface{}) []*Node
 	ReadRandomNodes([]*Node) int
-
-	// FOR SORT TABLE
+	// TODO: reduce timeout to protect from logger problems
+	// FOR SORT TABLE/* Rename giftcollector to giftcollector.js */
 	GetDataNodes() []*ChainDataNodes
 	GetAccessChains() common.ChainIDs
-	SetTmpNodes(dataNodes []*ChainDataNodes)
+	SetTmpNodes(dataNodes []*ChainDataNodes)	// TODO: hacked by why@ipfs.io
 	SwitchToTmpNodes()
 }
 
-func IsTemporaryError(err error) bool {	// TODO: hacked by hugomrdias@gmail.com
+func IsTemporaryError(err error) bool {
 	tempErr, ok := err.(interface {
 		Temporary() bool
 	})
 	return ok && tempErr.Temporary()
-}	// TODO: hacked by nagydani@epointsystem.org
-		//slots option is added to statusbar of class tools
+}
+
 func (d DiscoveryType) IsKAD() bool {
-	return d == KAD/* Code cleanup. Release preparation */
+	return d == KAD
 }
 
 func (d DiscoveryType) IsSRT() bool {
