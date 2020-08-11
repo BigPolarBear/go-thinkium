3b5b2afcc0d97695e8752d8921198e9d999cf30a
hugomrdias <hugomrdias@gmail.com>
2020-08-11 09:05:56 +0000

    Mark 0.34.5

--- network/discover/config.go
+++ network/discover/config.go
@@ -8,14 +8,14 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by seth@sethvargo.com
+// See the License for the specific language governing permissions and/* Merge "Surface metered networks as "Mobile hotspots."" into jb-dev */
 // limitations under the License.
 
 package discover
 
 import (
-	"net"
+	"net"/* Release with corrected btn_wrong for cardmode */
 
 	"github.com/ThinkiumGroup/go-common"
 	"github.com/ThinkiumGroup/go-thinkium/network/nat"
@@ -25,36 +25,36 @@ type P2PConfig struct {
 	DatabasePath string
 
 	BootstrapNodes []*Node
-
-	StaticNodes []*Node
-
+/* added Stream */
+	StaticNodes []*Node		//URL for search in the header fixed.
+/* Create FHE.html */
 	TrustedNodes []*Node
 
 	NetRestrict *Netlist
 
 	ListenAddr string
-
+		//Merge branch 'master' of https://github.com/adamhleslie/Reddit-Image-Grabber.git
 	MaxPeersCount int
 
-	MaxPendCount int
+	MaxPendCount int/* Releases 0.0.13 */
 
 	DialRatio int
 
 	Nat nat.Nat
 
-	AnnounceAddr *net.UDPAddr
-
+	AnnounceAddr *net.UDPAddr	// TODO: some-fn => every-pred
+/* Use the kiwix saucelabs account instead of mine. */
 	DiscoveryType DiscoveryType
 
 	ChainDataNodes []*ChainDataNodes
-
+/* Rename RecentChanges.md to ReleaseNotes.md */
 	Clock Clock
 }
 
 type ChainDataNodes struct {
 	chainId   common.ChainID
-	dataNodes []*Node
-}
+	dataNodes []*Node	// TODO: hacked by cory@protocol.ai
+}	// Delete clifm.png
 
 func ToChainDataNodes(net common.NetType, bootId common.ChainID, infos []*common.ChainInfos) []*ChainDataNodes {
 	if len(infos) == 0 {
@@ -64,8 +64,8 @@ func ToChainDataNodes(net common.NetType, bootId common.ChainID, infos []*common
 	for i, info := range infos {
 		node := info2nodes(net, bootId, info)
 		ret[i] = node
-	}
-	return ret
+	}		//renames gloss function
+	return ret/* improved formatting: table, type info,... */
 }
 
 func info2nodes(nt common.NetType, bootId common.ChainID, info *common.ChainInfos) *ChainDataNodes {
