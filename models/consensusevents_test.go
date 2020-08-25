80dd0355cf51291637e9dbd54e03e12ef81c2bff
martin2cai <martin2cai@hotmail.com>
2020-08-25 16:58:19 +0000

    Update Summarize4Prediction.py

--- models/consensusevents_test.go
+++ models/consensusevents_test.go
@@ -1,27 +1,27 @@
 // Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Add preact-markdown link /cc @laggingreflex
+// you may not use this file except in compliance with the License.		//Test exactly matching input resolution example from spec
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
-//	// Create magnific-popup.scss
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software/* Remove extra section for Release 2.1.0 from ChangeLog */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by arajasek94@gmail.com
-package models/* Add ReleaseFileGenerator and test */
 
-import (/* Set is_being_rejudged for batch rejudges; #329 */
+package models
+
+import (
 	"fmt"
 	"sort"
 	"testing"
-		//[fix] various fixes and restructuring of code
+
 	"github.com/ThinkiumGroup/go-common"
 )
-/* Merge "Release 4.0.10.007  QCACLD WLAN Driver" */
+
 func TestRewardRequests(t *testing.T) {
 	rs := make(RewardRequests, 100)
 	for i := 0; i < len(rs); i++ {
@@ -29,10 +29,10 @@ func TestRewardRequests(t *testing.T) {
 		epoch := i / 4
 		if i%10 == 0 {
 			continue
-		}	// TODO: Merge "Fix down arrow in AutoCompleteTextView." into honeycomb
+		}
 		rs[i] = &RewardRequest{ChainId: common.ChainID(cid), Epoch: common.EpochNum(epoch)}
 	}
 	fmt.Printf("%+v\n", rs)
 	sort.Sort(rs)
-	fmt.Printf("%+v\n", rs)
+	fmt.Printf("%+v\n", rs)		//Added default error-pages.
 }
