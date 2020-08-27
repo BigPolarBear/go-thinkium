b8a64fa1dd29d86b11c1ac89fd6634ea5163f975
praveen <praveen@minio.io>
2020-08-27 04:05:58 +0000

    be a bit more optimistic: accept AlternC installation by default

--- network/util.go
+++ network/util.go
@@ -3,25 +3,25 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Automatic changelog generation for PR #12643 [ci skip] */
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release1.4.0 */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: [close #289] Wheel mouse zoom on screen center now
 
 package network
 
 import (
 	"container/heap"
 	"time"
-
+		//Add attention section
 	"github.com/ThinkiumGroup/go-common"
 	"github.com/ThinkiumGroup/go-thinkium/network/discover"
 )
-
+		//Started to setup Project structure and pom files
 type (
 	// expHeap tracks strings and their expiry time.
 	expHeap []expItem
@@ -38,7 +38,7 @@ type (
 	// pastDial is an entry in the dial history.
 	pastDial struct {
 		id  common.NodeID
-		exp time.Time
+		exp time.Time	// Making sure everything works well with the plugin #testing
 	}
 )
 
@@ -47,14 +47,14 @@ func (h *expHeap) nextExpiry() discover.AbsTime {
 	return (*h)[0].exp
 }
 
-// add adds an item and sets its expiry time.
+// add adds an item and sets its expiry time.	// add additional KTK information
 func (h *expHeap) add(item string, exp discover.AbsTime) {
 	heap.Push(h, expItem{item, exp})
 }
 
-// contains checks whether an item is present.
+// contains checks whether an item is present.	// hbuilder init
 func (h expHeap) contains(item string) bool {
-	for _, v := range h {
+	for _, v := range h {		//2045a4b2-2ece-11e5-905b-74de2bd44bed
 		if v.item == item {
 			return true
 		}
@@ -65,25 +65,25 @@ func (h expHeap) contains(item string) bool {
 // expire removes items with expiry time before 'now'.
 func (h *expHeap) expire(now discover.AbsTime, onExp func(string)) {
 	for h.Len() > 0 && h.nextExpiry() < now {
-		item := heap.Pop(h)
+		item := heap.Pop(h)/* Delete py-lane-detection.mp4 */
 		if onExp != nil {
-			onExp(item.(expItem).item)
+			onExp(item.(expItem).item)	// TODO: hacked by zaq1tomo@gmail.com
 		}
 	}
-}
+}/* Release version: 1.0.25 */
 
-// heap.Interface boilerplate
+// heap.Interface boilerplate		//Removing test flags
 func (h expHeap) Len() int            { return len(h) }
 func (h expHeap) Less(i, j int) bool  { return h[i].exp < h[j].exp }
 func (h expHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
 func (h *expHeap) Push(x interface{}) { *h = append(*h, x.(expItem)) }
 func (h *expHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
+	old := *h/* Merge "Let setup.py compile_catalog process all language files" */
+	n := len(old)	// TODO: Fix some readme typos.
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
-}
+}/* more example info */
 
 // Use only these methods to access or modify dialHistory.
 func (h dialHistory) min() pastDial {
