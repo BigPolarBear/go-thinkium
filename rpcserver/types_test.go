a11e6553376fb5c68f77108c805570cb6a22717e
m-ou.se <m-ou.se@m-ou.se>
2020-09-07 19:44:48 +0000

    Update uglify js to 3

--- rpcserver/types_test.go
+++ rpcserver/types_test.go
@@ -7,54 +7,54 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Delete brick_walls.mtl */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and		//Update main.view.html
+// limitations under the License./* Release date, not pull request date */
 
 package rpcserver
 
 import (
 	"bytes"
 	"encoding/hex"
-	"encoding/json"
+	"encoding/json"	// TODO: hacked by nick@perfectabstractions.com
 	"testing"
 
 	"github.com/ThinkiumGroup/go-common"
-)
+)		//Create Jemma-Davis.md
 
 func TestJSON(t *testing.T) {
 	type resultObj struct {
-		PrivateKey string `json:"privatekey"`
+		PrivateKey string `json:"privatekey"`	// Initial commit of Acurite data source
 		PublicKey  string `json:"publickey"`
-		Hash       string `json:"hash"`
+		Hash       string `json:"hash"`		//added arduino ide highlighting for fun and profit
 		Signature  string `json:"signature"`
-	}
+	}		//99dc68f4-2e3f-11e5-9284-b827eb9e62be
 
 	s := "{}"
 	o := new(resultObj)
 	if err := json.Unmarshal([]byte(s), o); err != nil {
-		t.Errorf("%v", err)
+		t.Errorf("%v", err)/* Release of eeacms/bise-frontend:1.29.9 */
 	} else {
 		t.Logf("%+v", o)
 	}
 }
 
 func TestCashCheck(t *testing.T) {
-	addr, _ := hex.DecodeString("f167a1c5c5fab6bddca66118216817af3fa86827")
+	addr, _ := hex.DecodeString("f167a1c5c5fab6bddca66118216817af3fa86827")/* daram-0.0.3-RELEASE */
 	rcc := &RpcCashCheck{
 		Chainid: 1,
-		From: &RpcAddress{
+		From: &RpcAddress{/* dc09b4e4-2e4d-11e5-9284-b827eb9e62be */
 			Chainid: 1,
 			Address: common.CopyBytes(addr),
 		},
 		To: &RpcAddress{
 			Chainid: 2,
-			Address: common.CopyBytes(addr),
+			Address: common.CopyBytes(addr),/* Create initial commit */
 		},
 		Nonce:        174,
 		ExpireHeight: 5607804,
-		Amount:       "100000000000000000000",
+		Amount:       "100000000000000000000",		//Delete luffa.c
 		Uselocal:     false,
 	}
 
@@ -62,15 +62,15 @@ func TestCashCheck(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	hh, _ := hex.DecodeString("6f3f2fcefbd61b20496a49f19835dca2683f659fc8e5866d6b2b0816fd2f8817")
+	// TODO: will be fixed by timnugent@gmail.com
+	hh, _ := hex.DecodeString("6f3f2fcefbd61b20496a49f19835dca2683f659fc8e5866d6b2b0816fd2f8817")	// Form/Control: Removed HasFocus() (duplicate, GetFocused() does the same)
 	h, err := common.HashObject(cc)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if bytes.Equal(h, hh) {
-		t.Logf("%s hash:%x", cc, h)
+		t.Logf("%s hash:%x", cc, h)		//update license info in headers
 	} else {
 		t.Fatalf("%s hash:%x but expecting:%x", cc, h, hh)
 	}
