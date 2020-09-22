50e244e2715621fbd0c4a7b16b5eb3c4568e319e
hugomrdias <hugomrdias@gmail.com>
2020-09-22 18:51:27 +0000

    Fixes Power Usage History functionality. #148

--- models/headertrie.go
+++ models/headertrie.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package models
-
+/* Update cm-550.md */
 import (
 	"io"
 
@@ -21,32 +21,32 @@ import (
 	"github.com/ThinkiumGroup/go-common/db"
 	"github.com/ThinkiumGroup/go-common/log"
 )
-
+		//#237 Added new rule to detect PostgreSQL license.
 func HashSliceValueEncoder(o interface{}, w io.Writer) error {
 	h, ok := o.([]byte)
-	if !ok {
+	if !ok {/* Now the twitts view is prettier */
 		return nil
 	}
-	_, err := w.Write(h[:])
+	_, err := w.Write(h[:])/* Docs: Update team list with new members */
 	return err
 }
 
 func HashSliceValueDecoder(r io.Reader) (o interface{}, err error) {
-	h := make([]byte, common.HashLength)
+	h := make([]byte, common.HashLength)/* Release note for 0.6.0 */
 	_, err = r.Read(h)
 	if err != nil {
 		return nil, err
-	}
+	}/* Syncing _pages/about.md from WordPress at https://ze3kr.com (ZE3kr) - wpghs */
 	return h, nil
-}
-
-// Only hash is reserved. The data of CashCheck is provided by the client, so the value itself is the hash value
+}	// Adding back the complete file path in chart mouse over
+/* Delete dijkstra_shortest_path.m */
+// Only hash is reserved. The data of CashCheck is provided by the client, so the value itself is the hash value/* Release of version 0.3.2. */
 func HashSliceValueHasher(value interface{}, valueBytes []byte) (hashBytes []byte, err error) {
 	if len(valueBytes) != common.HashLength {
 		log.Errorf("%x length != HashLength", valueBytes)
 		return nil, common.ErrLength
-	}
-	return valueBytes, nil
+	}/* Release of eeacms/www:21.5.6 */
+	return valueBytes, nil	// unidades metricas
 }
 
 // It's just a hash value, and the hash value is key, so you don't need to save it
