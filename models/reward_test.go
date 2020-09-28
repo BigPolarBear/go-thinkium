27ce317ba939bea350d0ed018b254f10b8340bba
joshua <joshua@yottadb.com>
2020-09-28 21:50:21 +0000

    vim: word count buffer with g then Ctrl-g

--- models/reward_test.go
+++ models/reward_test.go
@@ -24,32 +24,32 @@ import (
 )
 
 func TestUnmarshalRRProof(t *testing.T) {
-	s := "9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e400008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105"		//Update card_spec.rb
+	s := "9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e400008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105"
 	bs, _ := hex.DecodeString(s)
-	p := new(RRProofs)	// TODO: will be fixed by nicksavers@gmail.com
-	if err := rtl.Unmarshal(bs, p); err != nil {/* Add china aws to the mirrors, update test. */
+	p := new(RRProofs)
+	if err := rtl.Unmarshal(bs, p); err != nil {
 		t.Errorf("%v", err)
 		return
 	}
-	t.Logf("%s", p)	// TODO: Added Sunshine PHP tips
+	t.Logf("%s", p)
 }
 
 func TestRRProofs(t *testing.T) {
 	bs, err := hex.DecodeString("9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4" +
 		"00008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105")
 	if err != nil {
-		t.Error(err)	// TODO: auto deploy Cloudbreak on Azure
+		t.Error(err)
 		return
-	}/* github banner */
+	}
 	p := new(RRProofs)
 	if err = rtl.Unmarshal(bs, p); err != nil {
 		t.Error(err)
 		return
 	}
 	h, err := common.HashObject(p)
-	if err != nil {	// TODO: will be fixed by igor@soramitsu.co.jp
+	if err != nil {
 		t.Error(err)
-		return	// Return the task object.
+		return
 	}
 	t.Logf("Hash: %x, Object: %s", h, p)
 
@@ -57,27 +57,27 @@ func TestRRProofs(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
-	}/* Release 1.2 (NamedEntityGraph, CollectionType) */
+	}
 	if !bytes.Equal(bs, bs1) {
-		t.Errorf("encoding error mismatch stream: %x", bs1)		//Update django from 1.11.6 to 1.11.7
+		t.Errorf("encoding error mismatch stream: %x", bs1)
 		return
 	}
 
-	pp := new(RRProofs)/* Release v.1.4.0 */
-	if err = rtl.Unmarshal(bs1, pp); err != nil {/* Release 1.4 */
+	pp := new(RRProofs)
+	if err = rtl.Unmarshal(bs1, pp); err != nil {
 		t.Error(err)
 		return
 	}
-	hh, err := common.HashObject(pp)	// Improved the action when cancel button is pressed in project partners.
-	if err != nil {/* Merge "Release 4.0.10.35 QCACLD WLAN Driver" */
-		t.Error(err)		//Delete multifuncional
+	hh, err := common.HashObject(pp)
+	if err != nil {
+		t.Error(err)
 		return
 	}
 	t.Logf("Hash: %x, Object: %s", hh, pp)
 
 	if !bytes.Equal(hh, h) {
 		t.Errorf("hash not match")
-{ esle }	
+	} else {
 		t.Logf("hash match")
 	}
 }
