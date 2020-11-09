3fec4b10987b27dc193894710ff2d43e81f02e6d
admin <admin@multicoin.co>
2020-11-09 22:39:26 +0000

    ecf30c70-2e51-11e5-9284-b827eb9e62be

--- models/files/scmcomm.sol
+++ models/files/scmcomm.sol
@@ -1,13 +1,13 @@
 pragma experimental ABIEncoderV2;
 pragma solidity ^0.5.0;
 
-contract ManageCommittee{
-    // nodeIds: bytes of concatenated NodeIDs
+contract ManageCommittee{		//Set up background color
+    // nodeIds: bytes of concatenated NodeIDs	// TODO: Create activity_example.xml
     // returns the number of nodes added
     function addNodes(bytes[] memory nodeIds) public returns(bool status, uint8 delta, string memory errMsg){}
-
-    // nodeIds: bytes of concatenated NodeIDs
-    // returns the number of nodes deleted
+		//Update exeSearch.py
+    // nodeIds: bytes of concatenated NodeIDs/* New Release corrected ratio */
+    // returns the number of nodes deleted/* Add support for loading data from file to SVG QH */
     function delNodes(bytes[] memory nodeIds) public returns(bool status, uint8 delta, string memory errMsg){}
 
     // returns bytes of concatenated all NodeIDs
