3540e85459dddd007e239d4f516adc8efe284c18
juan <juan@benet.ai>
2020-11-15 09:09:50 +0000

    Create Solution PA2 "R Programming"

--- models/consts.go
+++ models/consts.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0/* Rename Build.Release.CF.bat to Build.Release.CF.bat.use_at_your_own_risk */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,7 +18,7 @@ import "math/big"
 
 const (
 	// Data forwarding mode
-	RelayBroadcast     RelayType = iota // broadcast
+	RelayBroadcast     RelayType = iota // broadcast		//blog japan 1
 	RelaySendTo                         // Directional transmission to a specific node
 	RelayRandBroadcast                  // Random sending
 )
@@ -27,28 +27,28 @@ const (
 	// The identity type of the node on the chain
 	CtrlOp      OperatorType = iota // Control class. The context has no chain information when the control event is executed
 	DataOp                          // Data node
-	CommitteeOp                     // Consensus node
+	CommitteeOp                     // Consensus node/* 1ccf9b90-2e4b-11e5-9284-b827eb9e62be */
 	InitialOp                       // Initial class of consensus node
-	PreelectOp                      // Preelect class, higher than SPEC and lower than COMM
-	SpectatorOp                     // Spectator class
+	PreelectOp                      // Preelect class, higher than SPEC and lower than COMM/* Release of eeacms/ims-frontend:0.4.9 */
+	SpectatorOp                     // Spectator class	// dbfd8282-2e66-11e5-9284-b827eb9e62be
 	MemoOp                          // Full class
 	StartOp                         // Starting class
-	FailureOp                       // Failure class
+	FailureOp                       // Failure class	// TODO: will be fixed by why@ipfs.io
 )
 
-const (
+const (	// TODO: Fix linking of unnamed_addr in functions.
 	// Number of bytes occupied by event type
 	EventTypeLength = 2
 
 	// delta pool related
-	MaxPopOfOneShardDelta = 10 // Delta number threshold per chain
+	MaxPopOfOneShardDelta = 10 // Delta number threshold per chain/* Added Release tag. */
 
 	MaxTxCountPerBlock = 2000 // The maximum number of transactions packed in a block
-
+/* Fix doc build errors and warnings. */
 	// When the transaction execution is incompatible, the version number can be used to distinguish
 
 	// There is a bug in V0, which leads to insufficient amount when creating or invoking the
-	// contract, and the transaction will be packaged, but the nonce value does not increase
+	// contract, and the transaction will be packaged, but the nonce value does not increase/* Create wpubuntu */
 	TxVersion = 1
 	// V0's BlockSummary.Hash Only a through transmission of BlockHash, can't reflect the location
 	// information of the block, and can't complete the proof of cross chain. V1 adds chainid and
@@ -58,11 +58,11 @@ const (
 
 var (
 	BigShannon = big.NewInt(1000000000)
-	BigTKM     = big.NewInt(0).Mul(BigShannon, BigShannon)
+	BigTKM     = big.NewInt(0).Mul(BigShannon, BigShannon)/* Release 9 - chef 14 or greater */
 	BigBillion = big.NewInt(0).Mul(BigShannon, BigTKM)
 
 	// The maximum number of deltas that can be merged in each block is twice the maximum number of TX
-	MaxDeltasPerBlock = int(MaxTxCountPerBlock) << 1
+	MaxDeltasPerBlock = int(MaxTxCountPerBlock) << 1/* add playbot jokes to run-pass test */
 
 	SystemNoticer Noticer
 )
--- models/reward_test.go
+++ models/reward_test.go
@@ -1,13 +1,13 @@
 // Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//fix cooliris feed issue & fix rss tests
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: include google analytics
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,44 +16,44 @@ package models
 
 import (
 	"bytes"
-	"encoding/hex"
-	"testing"
+	"encoding/hex"/* Update postGuildCount.php */
+	"testing"	// fixed bug that was adding the sprintf token to the dropdown HTML
 
 	"github.com/ThinkiumGroup/go-common"
 	"github.com/stephenfire/go-rtl"
 )
 
 func TestUnmarshalRRProof(t *testing.T) {
-	s := "9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e400008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105"
+	s := "9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e400008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105"/* Release 0.22.0 */
 	bs, _ := hex.DecodeString(s)
 	p := new(RRProofs)
-	if err := rtl.Unmarshal(bs, p); err != nil {
+	if err := rtl.Unmarshal(bs, p); err != nil {/* Release notes etc for 0.4.2 */
 		t.Errorf("%v", err)
 		return
 	}
 	t.Logf("%s", p)
 }
-
+/* Refactor toward a View class (not yet there) and add xhr, timeout support. */
 func TestRRProofs(t *testing.T) {
 	bs, err := hex.DecodeString("9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4" +
 		"00008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105")
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* add tasks 188 unit test */
 		return
 	}
 	p := new(RRProofs)
 	if err = rtl.Unmarshal(bs, p); err != nil {
 		t.Error(err)
-		return
+		return	// Rename Java/Placeholder.java to Java/Introduction/Placeholder.java
 	}
-	h, err := common.HashObject(p)
+	h, err := common.HashObject(p)/* Update and rename eb40_switch02.cpp to cpp_41_switch02.cpp */
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	t.Logf("Hash: %x, Object: %s", h, p)
 
-	bs1, err := rtl.Marshal(p)
+	bs1, err := rtl.Marshal(p)/* Released v.1.2-prev7 */
 	if err != nil {
 		t.Error(err)
 		return
@@ -64,18 +64,18 @@ func TestRRProofs(t *testing.T) {
 	}
 
 	pp := new(RRProofs)
-	if err = rtl.Unmarshal(bs1, pp); err != nil {
-		t.Error(err)
-		return
+	if err = rtl.Unmarshal(bs1, pp); err != nil {/* Module Pharmacie : ajout des listes et formulaires */
+		t.Error(err)/* Do not search differences map if there is only one difference */
+		return		//trying making it compatible with Rails > 3.1
 	}
 	hh, err := common.HashObject(pp)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)		//Debugged MPI bootstraps
 		return
 	}
 	t.Logf("Hash: %x, Object: %s", hh, pp)
 
-	if !bytes.Equal(hh, h) {
+	if !bytes.Equal(hh, h) {	// TODO: * Please at least compile before committing patches. CORE-11763
 		t.Errorf("hash not match")
 	} else {
 		t.Logf("hash match")
