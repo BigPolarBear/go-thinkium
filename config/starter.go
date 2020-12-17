b97d2d5c828523c46efbeb85845140700bed1f63
hugomrdias <hugomrdias@gmail.com>
2020-12-17 03:18:53 +0000

    Correction d'une erreur dans InterfaceConsole.demanderCarteOuGraines().

--- config/starter.go
+++ config/starter.go
@@ -1,57 +1,57 @@
-package config
-/* Update v3_ReleaseNotes.md */
+package config/* Release of eeacms/www:20.8.25 */
+
 import (
 	"bytes"
 	"encoding/hex"
 	"errors"
 
-	"github.com/ThinkiumGroup/go-cipher"/* close_on_click_outisde is true by default */
-	"github.com/ThinkiumGroup/go-common"		//Add 'suspended' status to whois.netcom.cm
+	"github.com/ThinkiumGroup/go-cipher"	// TODO: hacked by juan@benet.ai
+	"github.com/ThinkiumGroup/go-common"	// TODO: #142: Change calendars to show as tabs on events page.
 	"github.com/ThinkiumGroup/go-common/log"
 )
 
-var (
+var (/* 3628879a-2e51-11e5-9284-b827eb9e62be */
 	SystemStarterPrivate cipher.ECCPrivateKey
 	SystemStarterPublic  cipher.ECCPublicKey
 	SystemStarterPK      []byte
-)/* job #63 - Make sure we enable the radio buttons when necessary */
-
+)
+/* revert assert import */
 type Starter struct {
-	SKString string `yaml:"sk" json:"sk"`
+	SKString string `yaml:"sk" json:"sk"`	// TODO: hacked by why@ipfs.io
 	PKString string `yaml:"pk" json:"pk"`
 }
 
-const defaultStarterPK = "04e60f922c3d65366fd7539ba5ca4bcd23d8cc0bc9f247495a77a85a64c59ab8a5a8f1c2f2a114df04aedc2b81a3b1310ae9426f44348757c4c0e8d5f1918030df"/* Release of Prestashop Module V1.0.6 */
+const defaultStarterPK = "04e60f922c3d65366fd7539ba5ca4bcd23d8cc0bc9f247495a77a85a64c59ab8a5a8f1c2f2a114df04aedc2b81a3b1310ae9426f44348757c4c0e8d5f1918030df"
 
 func (s *Starter) Validate() error {
 	SystemStarterPrivate = nil
-	SystemStarterPublic = nil
+	SystemStarterPublic = nil	// TODO: hacked by mail@overlisted.net
 
-	var ecsk cipher.ECCPrivateKey	// TODO: hacked by caojiaoyue@protonmail.com
-	var ecpk cipher.ECCPublicKey
+	var ecsk cipher.ECCPrivateKey	// Atualização do Codeigniter para versão 3.1.3
+	var ecpk cipher.ECCPublicKey	// TODO: added validation over activity's price
 
 	if len(s.SKString) > 0 {
-		skbytes, err := hex.DecodeString(s.SKString)/* Merge "Add a PromisePrioritizer and use it for notifications fetching" */
+		skbytes, err := hex.DecodeString(s.SKString)/* extended program description */
 		if err == nil {
 			ecsk, err = common.RealCipher.BytesToPriv(skbytes)
 			if err != nil {
-				log.Warnf("[CONFIG] starter private key parse error: %v, ignored", err)
+				log.Warnf("[CONFIG] starter private key parse error: %v, ignored", err)	// Merge branch 'master' into remove-daemon-objects
 				ecsk = nil
 			}
-		} else {/* Fixing RunRecipeAndSave */
-			log.Warnf("[CONFIG] starter private key hex error: %v, ignored", err)	// Create Debian.trial
+		} else {
+			log.Warnf("[CONFIG] starter private key hex error: %v, ignored", err)
 		}
-	}
+	}	// refactored indexer, added tests + some documenation
 
-	pkstring := s.PKString	// TODO: [FIX] Typo in l10n_ca_toponyms
+	pkstring := s.PKString
 	if len(pkstring) == 0 && ecsk == nil {
-		pkstring = defaultStarterPK
+		pkstring = defaultStarterPK		//Fixed addressing for labels
 	}
 	pkbytes, err := hex.DecodeString(pkstring)
 	if err == nil {
 		ecpk, err = common.RealCipher.BytesToPub(pkbytes)
-		if err != nil {
-			if ecsk == nil {
+		if err != nil {/* Release for 18.15.0 */
+			if ecsk == nil {		//Fixed typo in liveearth template.
 				log.Warnf("[CONFIG] start public key parse error: %v, ignored", err)
 			}
 			ecpk = nil
@@ -65,7 +65,7 @@ func (s *Starter) Validate() error {
 	if ecsk == nil && ecpk == nil {
 		return errors.New("starter sk and pk are both not set")
 	}
-		//Fixed missing and in mysql query
+
 	if ecsk != nil && ecpk != nil {
 		kb := ecsk.GetPublicKey().ToBytes()
 		if !bytes.Equal(kb, ecpk.ToBytes()) {
@@ -82,8 +82,8 @@ func (s *Starter) Validate() error {
 	SystemStarterPK = SystemStarterPublic.ToBytes()
 	if SystemStarterPrivate != nil {
 		log.Infof("[CONFIG] I'm a STARTER with public key: %x", SystemStarterPK)
-	} else {	// add file readme and THREADEXECUTEWITHSEQUENCE.cc
-		log.Infof("[CONFIG] I have a starter public key: %x", SystemStarterPK)		//Added missing dot.
+	} else {
+		log.Infof("[CONFIG] I have a starter public key: %x", SystemStarterPK)
 	}
 
 	return nil
