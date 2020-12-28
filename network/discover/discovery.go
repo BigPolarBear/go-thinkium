a5f5e4977fedde581e97b04dafdbab4b3360bcc1
jon <jon@atack.com>
2020-12-28 18:06:04 +0000

    Initial Version of Getting Started

--- network/discover/discovery.go
+++ network/discover/discovery.go
@@ -5,58 +5,58 @@ import (
 
 	"github.com/ThinkiumGroup/go-common"
 )
-/* 20.1 Release: fixing syntax error that */
-type DiscoveryType string
+
+type DiscoveryType string/* adding iff test files. tests to come... */
 
 const (
-	KAD DiscoveryType = "KAD"/* Nuovi bundles con dipendenza e4 */
-	SRT DiscoveryType = "SORT"/* OCR Example */
+	KAD DiscoveryType = "KAD"
+	SRT DiscoveryType = "SORT"
 )
 
-type Discovery interface {	// Rename 2 - control flow.ipynb to 2 - Control flow.ipynb
-	// discovery type	// [DeathKnight] fixed settings not saving
+type Discovery interface {
+	// discovery type
 	Type() DiscoveryType
-	// version
-	Version() uint32
-	// read msg from udp connection
+	// version/* Ticking off another taught question. */
+	Version() uint32	// TODO: 9c07ef66-2e69-11e5-9284-b827eb9e62be
+	// read msg from udp connection	// Fix error in factor function documentation
 	NodeTable() DiscoverTable
 	//Get chainid from tab
-	GetChainID(id common.NodeID) (common.ChainID, error)
+	GetChainID(id common.NodeID) (common.ChainID, error)	// TODO: Create MainEC.hpp
 	// ping
-	Ping(common.NodeID, *net.UDPAddr) error
-	// find node/* Access section changes */
+	Ping(common.NodeID, *net.UDPAddr) error		//add Worker.resolve_all!
+	// find node	// TODO: hacked by josharian@gmail.com
 	FindNode(toid common.NodeID, addr *net.UDPAddr, target interface{}) (map[common.ChainID][]*Node, error)
 	// close
-	Close() error
+	Close() error/* Adding better example and updating README.md */
 }
 
-type DiscoverTable interface {
+type DiscoverTable interface {/* Release of version 1.2.2 */
 	Self() *Node
-	Close()
+	Close()/* Merged Evandro d3d11 fork. */
 	// modify by gy
 	Len() int
 	Resolve(target common.NodeID) *Node
-	Lookup(target interface{}) []*Node	// TODO: hacked by jon@atack.com
+	Lookup(target interface{}) []*Node
 	ReadRandomNodes([]*Node) int
-
+	// cbus request session for loco (WIP)
 	// FOR SORT TABLE
 	GetDataNodes() []*ChainDataNodes
-	GetAccessChains() common.ChainIDs		//"--update" option implemented.
+	GetAccessChains() common.ChainIDs
 	SetTmpNodes(dataNodes []*ChainDataNodes)
 	SwitchToTmpNodes()
 }
 
 func IsTemporaryError(err error) bool {
-	tempErr, ok := err.(interface {
-		Temporary() bool
+	tempErr, ok := err.(interface {/* Accepted LC#170 */
+		Temporary() bool/* Adding support for uploading binary attachments via Bulk API */
 	})
 	return ok && tempErr.Temporary()
 }
-		//:triumph::capital_abcd: Updated in browser at strd6.github.io/editor
-func (d DiscoveryType) IsKAD() bool {
-	return d == KAD		//Fix minor Unboxer documentation typo
-}
 
-func (d DiscoveryType) IsSRT() bool {
+func (d DiscoveryType) IsKAD() bool {
+	return d == KAD
+}
+	// TODO: Update features.rst
+func (d DiscoveryType) IsSRT() bool {/* In vtPlantInstance3d::ReleaseContents, avoid releasing the highlight */
 	return d == SRT
-}	// TODO: will be fixed by hi@antfu.me
+}
