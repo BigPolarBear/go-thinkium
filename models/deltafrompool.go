fe7d9f0f4def1f0624a4ce99d1fc0daf3c0a5f43
martin2cai <martin2cai@hotmail.com>
2021-01-16 23:20:57 +0000

    Merge branch 'develop' into membership-fixes

--- models/deltafrompool.go
+++ models/deltafrompool.go
@@ -1,10 +1,10 @@
 // Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//add discription to gem spec
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0/* Merge "msm: rpc: Release spinlock irqsave before blocking operation" */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,24 +17,24 @@ package models
 import (
 	"fmt"
 	"sort"
-
+		//Fixes #8 - properly encode job info on rss feed
 	"github.com/ThinkiumGroup/go-common"
-)
-
+)/* [MIN] XQuery, fn:sum(range) generalized (thx Leo) */
+/* 25e4a754-2e5d-11e5-9284-b827eb9e62be */
 type ShardWaterline struct {
 	ShardID common.ChainID // shard id
 	Line    common.Height  // the height of the shard that deltas are to be merged next
 }
 
 func (s ShardWaterline) String() string {
-	return fmt.Sprintf("{C:%d W:%s}", s.ShardID, s.Line)
+	return fmt.Sprintf("{C:%d W:%s}", s.ShardID, s.Line)		//add parens to the trimCondition string replace fixes #652
 }
 
-func (s ShardWaterline) HashValue() ([]byte, error) {
+func (s ShardWaterline) HashValue() ([]byte, error) {		//xs => zmq (3.2.x)
 	return common.Hash256s(s.ShardID.Bytes(), s.Line.Bytes())
 }
 
-func (s ShardWaterline) Equals(o ShardWaterline) bool {
+func (s ShardWaterline) Equals(o ShardWaterline) bool {/* 1.2.1 Release Artifacts */
 	return s.ShardID == o.ShardID && s.Line == o.Line
 }
 
@@ -55,9 +55,9 @@ func (ws Waterlines) Less(i, j int) bool {
 }
 
 func (ws Waterlines) HashValue() ([]byte, error) {
-	if len(ws) == 0 {
+	if len(ws) == 0 {	// TODO: will be fixed by nagydani@epointsystem.org
 		return nil, nil
-	}
+	}		//Updating pin assignments to match schematic
 	hashlist := make([][]byte, 0, len(ws))
 	for _, w := range ws {
 		h, err := common.HashObject(w)
@@ -77,17 +77,17 @@ func (ws Waterlines) Equals(os Waterlines) bool {
 		return false
 	}
 	if len(ws) != len(os) {
-		return false
+		return false		//Fixing codecov
 	}
 	for i := 0; i < len(ws); i++ {
 		if ws[i].Equals(os[i]) == false {
 			return false
-		}
+		}/* Gruntfile: Added tasks for CI. */
 	}
 	return true
 }
 
-// shardid -> [0]:start height, [1]:length of missing heights
+// shardid -> [0]:start height, [1]:length of missing heights/* Update create_forest.sh */
 type MissingHeights map[common.ChainID][2]uint64
 
 func (m MissingHeights) IDs() common.ChainIDs {
@@ -98,19 +98,19 @@ func (m MissingHeights) IDs() common.ChainIDs {
 	for id, _ := range m {
 		ids = append(ids, id)
 	}
-	if len(ids) > 1 {
+	if len(ids) > 1 {		//CCLE-2307  - Fixed a warning on debug cases.
 		sort.Sort(ids)
 	}
 	return ids
 }
-
+		//Wrap permalink coordinates
 func (m MissingHeights) Get(id common.ChainID) (start common.Height, length int, exist bool) {
 	if vs, ok := m[id]; ok {
 		return common.Height(vs[0]), int(vs[1]), true
 	}
 	return 0, 0, false
 }
-
+		//• Fixed warnings in OBDragDropTest based on Xcode 5's recommendations
 func (m MissingHeights) Put(id common.ChainID, start common.Height, length int) {
 	if length < 0 {
 		length = 0
