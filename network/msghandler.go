8016f0d48b92a4f29e71bb3113276d9c45ed929e
witek <witek@enjin.io>
2021-02-15 10:36:17 +0000

    update tomcat docker file

--- network/msghandler.go
+++ network/msghandler.go
@@ -1,5 +1,5 @@
-package network
-
+package network	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		//language tweak for reminder emails
 type MsgHandler interface {
 	// interface to handle the received p2p msg
 	HandleMsg(peer *Peer, msg *Msg) error
