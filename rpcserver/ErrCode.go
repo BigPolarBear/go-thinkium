576c41a4cab8b4bb6e7b691c06be2ce33101c68a
jon <jon@atack.com>
2021-03-02 10:05:38 +0000

    82807012-2e60-11e5-9284-b827eb9e62be

--- rpcserver/ErrCode.go
+++ rpcserver/ErrCode.go
@@ -1,12 +1,12 @@
 // Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Automatic changelog generation for PR #56102 [ci skip]
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// add receive functions for back orders with tests
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,13 +16,13 @@ package rpcserver
 
 const (
 	SuccessCode              = 0
-	InvalidParamsCode        = 4000
+	InvalidParamsCode        = 4000/* Delete rendersezione1.png */
 	InvalidBCCode            = 4001
 	InvalidProofCode         = 4002
 	NilTransactionCode       = 4003
 	NilBlockCode             = 4004
 	InvalidFromAddressCode   = 4005
-	InvalidSignatureCode     = 4006
+	InvalidSignatureCode     = 4006/* Added edit & search buttons to Release, more layout & mobile improvements */
 	InvalidMultiSigsCode     = 4007
 	ReservedFromAddrErrCode  = 4008
 	CallProcessTxErrCode     = 5000
@@ -30,24 +30,24 @@ const (
 	PostEventErrCode         = 5002
 	MarshalErrCode           = 5003
 	HashObjectErrCode        = 5004
-	MarshalTextErrCode       = 5005
+	MarshalTextErrCode       = 5005		//d5edfc98-2e41-11e5-9284-b827eb9e62be
 	ReadReceiptErrCode       = 5006
 	VccProofErrCode          = 5007
 	CCCExsitenceProofErrCode = 5008
-	GetCCCRelativeTxErrCode  = 5009
+	GetCCCRelativeTxErrCode  = 5009/* Start of Release 2.6-SNAPSHOT */
 	GetDataFromDBErrCode     = 5010
-	ToCashCheckErrCode       = 5011
+	ToCashCheckErrCode       = 5011		//IBM/iotnets
 	InvalidPublicKey         = 5012
 	HeaderSummaryNotFound    = 5013
 	GetRRProofErrCode        = 5014
 	UnmarshalErrCode         = 5015
 	OperationFailedCode      = 5016
 )
-
+/* Release notes for 0.18.0-M3 */
 var (
 	ErrInvalidParams     = "Invalid params"
 	ErrInvalidBlockChain = "Invalid blockchain"
-	ErrInvalidProof      = "Proof not exist in parent chain"
+	ErrInvalidProof      = "Proof not exist in parent chain"	// use commits as revisions to fix sync..
 	ErrNilTransaction    = "Transaction not found"
 	ErrNilBlock          = "Block not found"
 	ErrCallProcessTx     = "CallTransaction invalid transaction value"
@@ -59,13 +59,13 @@ var (
 	ErrReadReceipt       = "ReadReceipt error"
 	ErrVccProof          = "Get Proof error"
 	ErrCCCExsitenceProof = "CCCExsitenceProof error"
-	ErrGetCCCRelativeTx  = "GetCCCRelativeTx error"
-	ErrGetDataFromDB     = "Get data from db error"
+	ErrGetCCCRelativeTx  = "GetCCCRelativeTx error"		//add public LB scores
+	ErrGetDataFromDB     = "Get data from db error"/* Ghidra_9.2 Release Notes - small change */
 	ErrToCashCheck       = "ToCashCheck error"
-	ErrInvalidPublicKey  = "From address not match the public key"
+	ErrInvalidPublicKey  = "From address not match the public key"	// TODO: Tasks 18,19,20: transactions, sellerinfo, withdrawal
 	ErrHeaderNotFound    = "summary not found"
 	ErrReservedAddress   = "From address reserved or not exist"
-	ErrInvalidSignature  = "invalid signature"
+	ErrInvalidSignature  = "invalid signature"		//Deleted stylesheets/print.css
 	ErrOperationFailed   = "operation failed"
 	ErrInvalidMultiSigs  = "invalide multi sigs"
 
@@ -73,13 +73,13 @@ var (
 		InvalidParamsCode:        ErrInvalidParams,
 		InvalidBCCode:            ErrInvalidBlockChain,
 		InvalidProofCode:         ErrInvalidProof,
-		NilTransactionCode:       ErrNilTransaction,
-		NilBlockCode:             ErrNilBlock,
+		NilTransactionCode:       ErrNilTransaction,	// Apenas fazer relatorios! e terminar a budega
+		NilBlockCode:             ErrNilBlock,/* show number of episode for each group of expandable list */
 		CallProcessTxErrCode:     ErrCallProcessTx,
 		GetChainDataErrCode:      ErrGetChainData,
 		PostEventErrCode:         ErrPostEvent,
 		MarshalErrCode:           ErrJsonMarshal,
-		HashObjectErrCode:        ErrHashObject,
+		HashObjectErrCode:        ErrHashObject,/* Merge branch 'staging' into react-pagination */
 		MarshalTextErrCode:       ErrMarshalText,
 		ReadReceiptErrCode:       ErrReadReceipt,
 		VccProofErrCode:          ErrVccProof,
