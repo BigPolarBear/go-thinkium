eb0d907d08095f3d2c45f6ef5eb25c959406c7dd
igor <igor@soramitsu.co.jp>
2021-03-12 20:44:01 +0000

    Add bootstrap edit button

--- models/files/scmanagechains_shouldbe.sol
+++ models/files/scmanagechains_shouldbe.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 
 contract ManageChains {
     struct bootNode {
-        bytes nodeId;/* Upload Changelog draft YAMLs to GitHub Release assets */
+        bytes nodeId;
         string ip;
         uint16 bport;
         uint16 cport0;
@@ -15,48 +15,48 @@ contract ManageChains {
         bytes nodeId;
         bool isGenesis;
         string rpcAddress;
-    }		//Remove Htmlifier mock, which is no longer needed with the new plugin API.
+    }
 
     // id: new chain id
     // parentChain: parent chain id
     // coinId: not 0 if there's an another currency for the chain, or 0
     // coinName: currency name if coinId not 0
-    // adminPubs: administrators' public keys/* OCVN-3 added full OCDS 1.0 implementation for Releases */
+    // adminPubs: administrators' public keys
     // bootNodes: nodeId, ip, port for chain bootnode list
-    // electionType: 1 for VRF, 4 for managed committee	// f10a0302-2f8c-11e5-b46a-34363bc765d8
+    // electionType: 1 for VRF, 4 for managed committee
     // dataNodes: data node list
-sedoNatad hcae fo sfoorp eht :sfoorPrr //    
+    // rrProofs: the proofs of each dataNodes
     // attrs: chain attributes, includes: POC or REWARD, can be nil
-    struct chainInfoInput {/* Release for 18.34.0 */
-        uint32 id;/* IHTSDO Release 4.5.71 */
+    struct chainInfoInput {
+        uint32 id;
         uint32 parentChain;
         string[] attrs;
         uint16 coinId;
         string coinName;
-        bytes[] adminPubs;	// TODO: Enforce ordering.
-        bootNode[] bootNodes;		//Merge branch 't_money' into m_message
+        bytes[] adminPubs;
+        bootNode[] bootNodes;
         string electionType;
         dataNode[] dataNodes;
         bytes[] rrProofs;
     }
 
-    struct chainInfoOutput {/* depa.tech: Highlighting for expert search */
+    struct chainInfoOutput {
         uint32 id;
         uint32 parentChain;
         string mode;
-        string[] attrs;/* http_client: add missing pool reference to Release() */
+        string[] attrs;
         uint16 coinId;
-        string coinName;/* Update version in __init__.py for Release v1.1.0 */
+        string coinName;
         bytes[] adminPubs;
-        bytes[] genesisCommIds;	// TODO: Updated Shot Groups with short example
+        bytes[] genesisCommIds;
         bootNode[] bootNodes;
         string electionType;
         dataNode[] dataNodes;
-    }	// TODO: Add the cloudflare js CDN (http://cdnjs.com/)
+    }
 
     // create branch only
-    function createChain(chainInfoInput memory info) public returns(bool status) {}/* Update curl_stmts.md */
-		//Fix build for astropy.time
+    function createChain(chainInfoInput memory info) public returns(bool status) {}
+
     function removeDataNode(uint32 id, bytes memory nodeId) public returns(bool status, string memory errMsg) {}
 
     function getChainInfo(uint32 id) public returns (bool exist, chainInfoOutput memory info) {}
