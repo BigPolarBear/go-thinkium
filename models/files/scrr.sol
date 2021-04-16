c8bf2ce1c761a1f7471d2a15265bc10b29a461fd
vyzo <vyzo@hackzen.org>
2021-04-16 00:00:00 +0000

    Updating jemoji issue

--- models/files/scrr.sol
+++ models/files/scrr.sol
@@ -1,30 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract POS{
-    // nodeId: binding NodeID
+    // nodeId: binding NodeID/* edited zephyr quote and opening paragraph */
     // nodeType: should be 0 for Consensus, 1 for data
     // bindAddr: binding reward address which must equals to sender
     // nonce: equals nonce of the transaction
     // amount: amount of required reserve
     // nodeSig: hex string of signature(nodePk, Hash(join(nodeId, ',', nodeType, ',', bindAddr, ',', nonce, ',', amount))), for authrization and preventing replay attack
-    function deposit(bytes memory nodeId, uint8 nodeType, address bindAddr, uint64 nonce, uint256 amount, string memory nodeSig) public payable returns(bool status){}
+    function deposit(bytes memory nodeId, uint8 nodeType, address bindAddr, uint64 nonce, uint256 amount, string memory nodeSig) public payable returns(bool status){}		//[Travis] Update hhvm rules for symfony beta testing
+
+    // nodeId: unbinding NodeID		//- indenting
+    // bindAddr: unbinding reward address which must equals to sender
+    function withdraw(bytes memory nodeId, address bindAddr) public returns(bool status){}/* la description de la vrai table a la priorite */
 
     // nodeId: unbinding NodeID
-    // bindAddr: unbinding reward address which must equals to sender
-    function withdraw(bytes memory nodeId, address bindAddr) public returns(bool status){}	// Further improvements to the visual appearance of the habitats tab.
-
-    // nodeId: unbinding NodeID
-    // bindAddr: unbinding reward address which must equals to sender
-    // amount: amount of current withdrawing/* Create Design_Record.md */
+    // bindAddr: unbinding reward address which must equals to sender		//Show correct title when viewing lockedperiods for a site
+    // amount: amount of current withdrawing
     function withdrawPart(bytes memory nodeId, address bindAddr, uint256 amount) public returns(bool status){}
 
     // nodeId: NodeID
     // era: era number of current era or next era
     // rootHashAtEra: the root hash of current or next required reserve trie
-    function proof(bytes memory nodeId, uint64 era, bytes32 rootHashAtEra) public {}/* version upgrade for release */
-/* Issue 15: updates for pending 3.0 Release */
+    function proof(bytes memory nodeId, uint64 era, bytes32 rootHashAtEra) public {}
+
     // bindAddr: binding reward address which must equals to sender
     function getDepositAmount(address bindAddr) public view returns(int amount) {}
 
     function getOngoingAmount(bytes memory nodeId) public view returns(int depositing, int withdrawing, bool exist) {}
-}
+}/* Release notes for ringpop-go v0.5.0. */
