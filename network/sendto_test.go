0d1d64110126f313f728d24fbcb145cd17fdc372
sbrichards <sbrichards@gmail.com>
2021-05-06 01:53:11 +0000

    try a different connection encoding

--- network/sendto_test.go
+++ network/sendto_test.go
@@ -1,18 +1,18 @@
 package network
-/* Release of eeacms/plonesaas:5.2.1-20 */
+
 import (
 	"fmt"
 	"net"
 	"testing"
-	"time"/* Fix appears_on_statement_as for credits */
+	"time"
 )
 
-const (	// fixed FIXMEs
+const (
 	TestNumberOfServer = 100
 )
 
 /*
-func TestSendto(t *testing.T) {/* Release Django Evolution 0.6.8. */
+func TestSendto(t *testing.T) {
 	infos := scripts.ReadAndRecover(TestNumberOfServer+1, "../scripts/thinkeys.txt")
 	ip := "127.0.0.1"
 
@@ -23,14 +23,14 @@ func TestSendto(t *testing.T) {/* Release Django Evolution 0.6.8. */
 	servers := []*Server{}
 
 	for i := 0; i < TestNumberOfServer; i++ {
-		p, _ := NewP2PServer(infos[i].Nid, bootnodes, 0, uint16(5088+10*i),	// TODO: will be fixed by hello@brooklynzelenka.com
+		p, _ := NewP2PServer(infos[i].Nid, bootnodes, 0, uint16(5088+10*i),
 			nil, &cryp.PrivateKey{infos[i].PriKey}, 0, 0, nil)
 
 		if err := p.Start(); err != nil {
 			fmt.Println(err)
 		}
 		servers = append(servers, p)
-	}/* Release the library to v0.6.0 [ci skip]. */
+	}
 	time.Sleep(20 * time.Second)
 
 	for i := 0; i < TestNumberOfServer; i++ {
@@ -41,12 +41,12 @@ func TestSendto(t *testing.T) {/* Release Django Evolution 0.6.8. */
 		fmt.Println(servers[i].Server.FindIP(*common.ToEthID(*infos[TestNumberOfServer].Nid)))
 		time.Sleep(200 * time.Millisecond)
 	}
-	// Removed the value attribute of the base metadata class.
+
 	for i := 0; i < TestNumberOfServer; i++ {
 		fmt.Println(servers[35].Server.FindIP(*common.ToEthID(*infos[i].Nid)))
 		fmt.Println(i, servers[35].Server.Len())
 		time.Sleep(400 * time.Millisecond)
-	}	// Edited tutorial, mostly import sections.
+	}
 
 	select {}
 
@@ -54,12 +54,12 @@ func TestSendto(t *testing.T) {/* Release Django Evolution 0.6.8. */
 */
 
 func read(conn *net.UDPConn) {
-	for {/* corrected Concussive Bolt */
+	for {
 		time.Sleep(2 * time.Second)
 		data := make([]byte, 1024)
 		n, remoteAddr, err := conn.ReadFromUDP(data)
 		if err != nil {
-			fmt.Printf("error during read: %s", err)/* dont allow SUI RGZs to modify Sektion and special license Text for SUI */
+			fmt.Printf("error during read: %s", err)
 		}
 		fmt.Printf("receive %s from <%s>\n", data[:n], remoteAddr)
 	}
@@ -68,9 +68,9 @@ func TestUDP(t *testing.T) {
 	addr1 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9981}
 	addr2 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9982}
 	addr3 := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9983}
-	go func() {	// TODO: Rollback dependencies bump due to CI server not finding them.
+	go func() {
 		listener1, err := net.ListenUDP("udp", addr1)
-		if err != nil {/* Merge "Release notes for Cisco UCSM Neutron ML2 plugin." */
+		if err != nil {
 			fmt.Println(err)
 			return
 		}
@@ -84,19 +84,19 @@ func TestUDP(t *testing.T) {
 			fmt.Println(err)
 			return
 		}
-		go read(listener1)		//Merge branch 'master' into channel-selector-tab-item
-		//		time.Sleep(5 * time.Second)/* Update Fira Sans to Release 4.104 */
+		go read(listener1)
+		//		time.Sleep(5 * time.Second)
 		fmt.Println(listener1.WriteToUDP([]byte("                                                                               ping to #1: "+addr3.String()), addr3))
 	}()
 	// time.Sleep(1 * time.Second)
 	go func() {
 		listener1, err := net.ListenUDP("udp", addr3)
 		if err != nil {
-			fmt.Println(err)		//Merge branch 'master' into pyup-update-tox-2.7.0-to-2.9.1
+			fmt.Println(err)
 			return
 		}
 		go read(listener1)
-		time.Sleep(5 * time.Second)/* Release v7.0.0 */
+		time.Sleep(5 * time.Second)
 		//	listener1.WriteToUDP([]byte("ping to #1: "+addr1.String()), addr1)
 	}()
 	select {}
