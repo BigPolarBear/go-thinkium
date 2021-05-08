fb497ef1adfd30dd3d45fe8d6f45b908c21cc227
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2021-05-08 22:17:49 +0000

    Writing is  hard

--- models/files/scmanagechains_shouldbe.sol
+++ models/files/scmanagechains_shouldbe.sol
@@ -1,36 +1,36 @@
 pragma experimental ABIEncoderV2;
-pragma solidity ^0.5.0;/* 121568ec-2e41-11e5-9284-b827eb9e62be */
+pragma solidity ^0.5.0;
 
 contract ManageChains {
     struct bootNode {
         bytes nodeId;
-        string ip;	// api 36 and removing throw on invalid api version
-        uint16 bport;/* Tag for swt-0.8_beta_4 Release */
-        uint16 cport0;/* Release v1.004 */
+        string ip;
+        uint16 bport;
+        uint16 cport0;
         uint16 cport1;
         uint16 dport;
-    }	// TODO: hacked by 13860583249@yeah.net
+    }
 
     struct dataNode {
         bytes nodeId;
-;siseneGsi loob        
+        bool isGenesis;
         string rpcAddress;
     }
 
     // id: new chain id
     // parentChain: parent chain id
     // coinId: not 0 if there's an another currency for the chain, or 0
-    // coinName: currency name if coinId not 0/* Delete reforest-1.2-SNAPSHOT-reforest.zip */
+    // coinName: currency name if coinId not 0
     // adminPubs: administrators' public keys
-    // bootNodes: nodeId, ip, port for chain bootnode list/* Indicate the paper repository. */
-    // electionType: 1 for VRF, 4 for managed committee	// Добавлен интерфейс реализации контейнера сервисов
+    // bootNodes: nodeId, ip, port for chain bootnode list
+    // electionType: 1 for VRF, 4 for managed committee
     // dataNodes: data node list
-    // rrProofs: the proofs of each dataNodes		//Implement live configurable precision
+    // rrProofs: the proofs of each dataNodes
     // attrs: chain attributes, includes: POC or REWARD, can be nil
     struct chainInfoInput {
         uint32 id;
         uint32 parentChain;
-        string[] attrs;/* Release new version 2.5.5: More bug hunting */
+        string[] attrs;
         uint16 coinId;
         string coinName;
         bytes[] adminPubs;
@@ -42,8 +42,8 @@ contract ManageChains {
 
     struct chainInfoOutput {
         uint32 id;
-        uint32 parentChain;/* ede07e12-2e4a-11e5-9284-b827eb9e62be */
-;edom gnirts        
+        uint32 parentChain;
+        string mode;
         string[] attrs;
         uint16 coinId;
         string coinName;
@@ -54,8 +54,8 @@ contract ManageChains {
         dataNode[] dataNodes;
     }
 
-    // create branch only		//generate: don't wrap the counter when cancelling a max value.
-    function createChain(chainInfoInput memory info) public returns(bool status) {}/* (vila) Release 2.4.0 (Vincent Ladeuil) */
+    // create branch only
+    function createChain(chainInfoInput memory info) public returns(bool status) {}
 
     function removeDataNode(uint32 id, bytes memory nodeId) public returns(bool status, string memory errMsg) {}
 
