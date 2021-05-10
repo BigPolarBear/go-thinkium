be38cfc2b370060c2dc14e4ec31269484b6596bf
juan <juan@benet.ai>
2021-05-10 11:05:40 +0000

    Merge "Adding log to db_sync"

--- cmd/cmds_admin.go
+++ cmd/cmds_admin.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Thinkium	// TODO: hacked by arachnid@notdot.net
+// Copyright 2020 Thinkium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,16 +7,16 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Add note about curl ca-certs */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Lo arruiné :( */
-package cmd/* Release version [10.7.0] - alfter build */
+
+package cmd
 
 func init() {
 	AllCommands.Put(
 		&start{"start"},
 		&stop{"stop"},
-	)/* ReleaseTag: Version 0.9 */
+	)
 }
