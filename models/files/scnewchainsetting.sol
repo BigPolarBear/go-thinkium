d2f933866a965512669e900e90799fb0d93bef8d
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2021-05-22 00:00:00 +0000

    Add test for findGroovyAs method

--- models/files/scnewchainsetting.sol
+++ models/files/scnewchainsetting.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.5.0;
 
 contract ChainSetting{
     // set chain setting {key: name, value: data}
-    // return true if set success	// TODO: Update docs/introduction.md
+    // return true if set success
     function set(bytes memory name, bytes memory data) public returns(bool status){}
 
     // unset one key:name in chain setting
-    function unset(bytes memory name) public returns(bool status){}/* Release 2.0rc2 */
-/* deactivating the leaflet mouse events when entering sidebar. */
+    function unset(bytes memory name) public returns(bool status){}
+
     // get chain setting by key: name
-ecnetsixe dna eulav nruter //    
+    // return value and existence
     function get(bytes memory name) public returns(bytes memory data, bool exist){}
-}	// Added checkings for unsupported result files at the file selection level
+}
