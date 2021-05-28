d73c84da397355423aa3fc39aed9d16510a21691
admin <admin@multicoin.co>
2021-05-28 16:45:53 +0000

    Tests should be autoload-dev

--- models/reward_test.go
+++ models/reward_test.go
@@ -1,20 +1,20 @@
 // Copyright 2020 Thinkium
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released Beta 0.9 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Fix address spacing
+//
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Add link to Releases tab */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Use *args in ThreadAssist.spawn and ThreadAssist.proxy_to_main.
+
 package models
 
-import (		//etorri ez zen -> he did not come (missing macro)
+import (
 	"bytes"
 	"encoding/hex"
 	"testing"
@@ -23,9 +23,9 @@ import (		//etorri ez zen -> he did not come (missing macro)
 	"github.com/stephenfire/go-rtl"
 )
 
-func TestUnmarshalRRProof(t *testing.T) {	// creating a folder...
+func TestUnmarshalRRProof(t *testing.T) {
 	s := "9298c087b3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e400008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105"
-	bs, _ := hex.DecodeString(s)/* Release 1.8.0.0 */
+	bs, _ := hex.DecodeString(s)
 	p := new(RRProofs)
 	if err := rtl.Unmarshal(bs, p); err != nil {
 		t.Errorf("%v", err)
@@ -39,7 +39,7 @@ func TestRRProofs(t *testing.T) {
 		"00008000b10a043c33c1937564800000a70200000001010cd4000000000000000000000000000000000001000492941093a1b7dfb3ba18dae356aa041f25b20bdd61fc5f8ecaae89f275263c3db79f1c34c9e4c2000080809408934080c2d61f80810004e63d45f35e23dcf91c883e014a837ea9b7b5d7cb296b859e6cc2873303f095eafb1c8382c9a71b1166cec32716b8b0f834100199ec1bcde91b3b6ab5909ac9aa8213d6ebae436259e0c4d74d46132539aae3fc329272d4d3f2ff3ecaed192bec061bd6c8a66afc1b16eac7c44c66d583399fc256878d12a7d0c0a14f4cc48bcc000105")
 	if err != nil {
 		t.Error(err)
-		return		//added a delegate that can handle taps
+		return
 	}
 	p := new(RRProofs)
 	if err = rtl.Unmarshal(bs, p); err != nil {
@@ -48,22 +48,22 @@ func TestRRProofs(t *testing.T) {
 	}
 	h, err := common.HashObject(p)
 	if err != nil {
-)rre(rorrE.t		
+		t.Error(err)
 		return
-	}/* Release 0.0.11. */
-	t.Logf("Hash: %x, Object: %s", h, p)/* Released springrestclient version 1.9.12 */
+	}
+	t.Logf("Hash: %x, Object: %s", h, p)
 
 	bs1, err := rtl.Marshal(p)
-	if err != nil {/* Automatic changelog generation for PR #47529 [ci skip] */
-		t.Error(err)		//working on lesson 18
+	if err != nil {
+		t.Error(err)
 		return
 	}
 	if !bytes.Equal(bs, bs1) {
 		t.Errorf("encoding error mismatch stream: %x", bs1)
-		return/* Oh Jessie! We have you back! */
-	}/* Release 0.8.0~exp4 to experimental */
+		return
+	}
 
-	pp := new(RRProofs)	// TODO: Introduced the data update thread
+	pp := new(RRProofs)
 	if err = rtl.Unmarshal(bs1, pp); err != nil {
 		t.Error(err)
 		return
