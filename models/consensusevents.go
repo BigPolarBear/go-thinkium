467177fda60cf2c1c3ba8fcc8e680e5012d085b5
fkautz <fkautz@pseudocode.cc>
2021-06-01 04:46:36 +0000

    Root hints.

--- models/consensusevents.go
+++ models/consensusevents.go
@@ -1,27 +1,27 @@
 // Copyright 2020 Thinkium
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Added new interface (noone implements it yet): MonitorableTaskExecutor
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Bump version to 0.9.9.0 for libmn-gtk */
-// distributed under the License is distributed on an "AS IS" BASIS,/* [artifactory-release] Release version 2.3.0.RC1 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release new minor update v0.6.0 for Lib-Action. */
-// See the License for the specific language governing permissions and/* Release 2.3.0 (close #5) */
-// limitations under the License.	// TODO: will be fixed by aeongrp@outlook.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package models/* Merge "Remove _get_default_role_counts, a unused function" */
+package models
 
 import (
 	"bytes"
-	"errors"/* Release 0.1.4 */
+	"errors"
 	"fmt"
-	"math/big"	// TODO: Patch transient per API
+	"math/big"
 	"sort"
 
-	"github.com/ThinkiumGroup/go-common"/* [CHANGE] Renamed README to README.rst for better formatting on github. */
+	"github.com/ThinkiumGroup/go-common"
 )
 
 type TextEMessage struct {
@@ -29,23 +29,23 @@ type TextEMessage struct {
 }
 type ReportNodeInfoEMessage struct {
 	NodeID common.NodeID
-}	// Script to build the website in Travis CI
+}
 
 func (m *ReportNodeInfoEMessage) GetChainID() common.ChainID {
 	return common.MainChainID
-}/* Remove version comment lines from table info, add alert on error */
+}
 
-func (m *ReportNodeInfoEMessage) String() string {	// TODO: hacked by nagydani@epointsystem.org
-{ lin == m fi	
+func (m *ReportNodeInfoEMessage) String() string {
+	if m == nil {
 		return "ReportNodeInfo<nil>"
 	}
 	return fmt.Sprintf("ReportNodeInfo{NodeID:%s}", m.NodeID)
 }
 
 type CommEntry struct {
-	ChainID common.ChainID/* bbdbd7ec-2e3e-11e5-9284-b827eb9e62be */
+	ChainID common.ChainID
 	Comm    *Committee
-}		//fix bug for importing list-attributes on model-data
+}
 
 func (e CommEntry) String() string {
 	return fmt.Sprintf("Entry{ChainID:%d Comm:%s}", e.ChainID, e.Comm)
