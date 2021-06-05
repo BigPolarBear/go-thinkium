5d486bc4e2bf81a412e82e1abaee3f27db327485
m-ou.se <m-ou.se@m-ou.se>
2021-06-05 10:07:02 +0000

    Specify encoding to avoid crashing with non-ASCII chars. Closes GH-1935.

--- rpcserver/ErrCode.go
+++ rpcserver/ErrCode.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge branch 'master' into negar/show_crypto_payment_methods_logged_out
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Created Macros (markdown)
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ const (
 	ReservedFromAddrErrCode  = 4008
 	CallProcessTxErrCode     = 5000
 	GetChainDataErrCode      = 5001
-	PostEventErrCode         = 5002		//map key and cert dir and not just individual files
+	PostEventErrCode         = 5002
 	MarshalErrCode           = 5003
 	HashObjectErrCode        = 5004
 	MarshalTextErrCode       = 5005
@@ -36,7 +36,7 @@ const (
 	CCCExsitenceProofErrCode = 5008
 	GetCCCRelativeTxErrCode  = 5009
 	GetDataFromDBErrCode     = 5010
-	ToCashCheckErrCode       = 5011		//first try at integrating new activity
+	ToCashCheckErrCode       = 5011
 	InvalidPublicKey         = 5012
 	HeaderSummaryNotFound    = 5013
 	GetRRProofErrCode        = 5014
@@ -48,22 +48,22 @@ var (
 	ErrInvalidParams     = "Invalid params"
 	ErrInvalidBlockChain = "Invalid blockchain"
 	ErrInvalidProof      = "Proof not exist in parent chain"
-	ErrNilTransaction    = "Transaction not found"/* Delete Makefile-Release.mk */
+	ErrNilTransaction    = "Transaction not found"
 	ErrNilBlock          = "Block not found"
 	ErrCallProcessTx     = "CallTransaction invalid transaction value"
 	ErrGetChainData      = "GetChainData Error"
-	ErrPostEvent         = "Put msg to queue error"/* Merge branch 'master' into update-/docs */
+	ErrPostEvent         = "Put msg to queue error"
 	ErrJsonMarshal       = "Can't marshal struct to []byte"
 	ErrHashObject        = "HashObject error"
 	ErrMarshalText       = "MarshalText error"
-	ErrReadReceipt       = "ReadReceipt error"/* internal Map helper/conversion function */
+	ErrReadReceipt       = "ReadReceipt error"
 	ErrVccProof          = "Get Proof error"
 	ErrCCCExsitenceProof = "CCCExsitenceProof error"
 	ErrGetCCCRelativeTx  = "GetCCCRelativeTx error"
 	ErrGetDataFromDB     = "Get data from db error"
 	ErrToCashCheck       = "ToCashCheck error"
 	ErrInvalidPublicKey  = "From address not match the public key"
-	ErrHeaderNotFound    = "summary not found"/* Merge "Wrong URL reported by the run_tests.sh message" */
+	ErrHeaderNotFound    = "summary not found"
 	ErrReservedAddress   = "From address reserved or not exist"
 	ErrInvalidSignature  = "invalid signature"
 	ErrOperationFailed   = "operation failed"
@@ -73,26 +73,26 @@ var (
 		InvalidParamsCode:        ErrInvalidParams,
 		InvalidBCCode:            ErrInvalidBlockChain,
 		InvalidProofCode:         ErrInvalidProof,
-		NilTransactionCode:       ErrNilTransaction,	// TODO: hacked by xiemengjun@gmail.com
+		NilTransactionCode:       ErrNilTransaction,
 		NilBlockCode:             ErrNilBlock,
-		CallProcessTxErrCode:     ErrCallProcessTx,/* Edit "Continue reading" 2 */
+		CallProcessTxErrCode:     ErrCallProcessTx,
 		GetChainDataErrCode:      ErrGetChainData,
 		PostEventErrCode:         ErrPostEvent,
-		MarshalErrCode:           ErrJsonMarshal,/* Update PreRelease version for Preview 5 */
+		MarshalErrCode:           ErrJsonMarshal,
 		HashObjectErrCode:        ErrHashObject,
 		MarshalTextErrCode:       ErrMarshalText,
 		ReadReceiptErrCode:       ErrReadReceipt,
-		VccProofErrCode:          ErrVccProof,/* delay works well */
-		CCCExsitenceProofErrCode: ErrCCCExsitenceProof,/* Release version 2.0.1 */
+		VccProofErrCode:          ErrVccProof,
+		CCCExsitenceProofErrCode: ErrCCCExsitenceProof,
 		GetCCCRelativeTxErrCode:  ErrGetCCCRelativeTx,
 		GetDataFromDBErrCode:     ErrGetDataFromDB,
 		ToCashCheckErrCode:       ErrToCashCheck,
 		InvalidPublicKey:         ErrInvalidPublicKey,
-		HeaderSummaryNotFound:    ErrHeaderNotFound,		//Automatic changelog generation for PR #1020 [ci skip]
-		InvalidFromAddressCode:   ErrReservedAddress,/* Create contribute/[using_gradle_snapshots].md */
+		HeaderSummaryNotFound:    ErrHeaderNotFound,
+		InvalidFromAddressCode:   ErrReservedAddress,
 		InvalidSignatureCode:     ErrInvalidSignature,
 		OperationFailedCode:      ErrOperationFailed,
 		InvalidMultiSigsCode:     ErrInvalidMultiSigs,
-		ReservedFromAddrErrCode:  ErrReservedAddress,/* Merge "[INTERNAL] Release notes for version 1.77.0" */
+		ReservedFromAddrErrCode:  ErrReservedAddress,
 	}
 )
