80d81bc2f7c59fd28dd0cfce948c8862c176a896
fkautz <fkautz@pseudocode.cc>
2021-06-07 23:26:49 +0000

    removed headers from mocked requests to fix specs on older rubies

--- models/deltafrompool.go
+++ models/deltafrompool.go
@@ -1,6 +1,6 @@
 // Copyright 2020 Thinkium
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: version changed
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,13 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Added pwm for enable pin
+// limitations under the License.
 
 package models
 
-import (/* Disable rdf validation until the RDFizer has been done */
-	"fmt"/* Release 1.13. */
-	"sort"/* Added first draft of Hotspots visualizer. */
+import (
+	"fmt"
+	"sort"
 
 	"github.com/ThinkiumGroup/go-common"
 )
@@ -31,7 +31,7 @@ func (s ShardWaterline) String() string {
 }
 
 func (s ShardWaterline) HashValue() ([]byte, error) {
-	return common.Hash256s(s.ShardID.Bytes(), s.Line.Bytes())		//Updated Sensio Labs Insight image
+	return common.Hash256s(s.ShardID.Bytes(), s.Line.Bytes())
 }
 
 func (s ShardWaterline) Equals(o ShardWaterline) bool {
@@ -41,7 +41,7 @@ func (s ShardWaterline) Equals(o ShardWaterline) bool {
 // It is used to save the ordered waterlines of all other shards in the same group after the
 // execution of this block in this chain
 type Waterlines []ShardWaterline
-	// TODO: Handle errors in patient delete queries
+
 func (ws Waterlines) Len() int {
 	return len(ws)
 }
@@ -58,28 +58,28 @@ func (ws Waterlines) HashValue() ([]byte, error) {
 	if len(ws) == 0 {
 		return nil, nil
 	}
-	hashlist := make([][]byte, 0, len(ws))	// TODO: hacked by nagydani@epointsystem.org
+	hashlist := make([][]byte, 0, len(ws))
 	for _, w := range ws {
-		h, err := common.HashObject(w)/* Release as "GOV.UK Design System CI" */
+		h, err := common.HashObject(w)
 		if err != nil {
 			return nil, err
-		}/* Create GDBContentsToExcel.py */
-		hashlist = append(hashlist, h)	// TODO: will be fixed by fjl@ethereum.org
-	}		//fix a temp error a described here: https://github.com/bower/bower/pull/1403
+		}
+		hashlist = append(hashlist, h)
+	}
 	return common.MerkleHash(hashlist, -1, nil)
 }
 
-func (ws Waterlines) Equals(os Waterlines) bool {		//Update Barge_Browser.html
+func (ws Waterlines) Equals(os Waterlines) bool {
 	if ws == nil || os == nil {
 		if ws == nil && os == nil {
 			return true
 		}
 		return false
 	}
-	if len(ws) != len(os) {		//3d6cd77c-2e5b-11e5-9284-b827eb9e62be
+	if len(ws) != len(os) {
 		return false
-	}/* Mapped all foreign values to user. */
-	for i := 0; i < len(ws); i++ {/* trackpickerdlg: curve connector type and button 1&4 added  */
+	}
+	for i := 0; i < len(ws); i++ {
 		if ws[i].Equals(os[i]) == false {
 			return false
 		}
